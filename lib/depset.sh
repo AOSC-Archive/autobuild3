f86303cf8d696bf364f2773c5bf39fcf569f3621
Icenowy Zheng <icenowylin@gmail.com>
2014-05-17 22:49:05 +0800

    depset

---
+++ lib/depset.sh
@@ -0,0 +1,17 @@
+abreqexe grep
+depset_chk(){
+	(echo $PKGDEP | grep "^$1 " > /dev/null) && return 0
+	(echo $PKGDEP | grep " $1\$" > /dev/null) && return 0
+	(echo $PKGDEP | grep " $1 " > /dev/null) && return 0
+	(echo $PKGDEP | grep "^$1|" > /dev/null) && return 0
+	(echo $PKGDEP | grep "|$1\$" > /dev/null) && return 0
+	(echo $PKGDEP | grep "|$1 " > /dev/null) && return 0
+	(echo $PKGDEP | grep " $1|" > /dev/null) && return 0
+	(echo $PKGDEP | grep "^$1\$" > /dev/null) && return 0
+	(echo $PKGDEP | grep "|$1|" > /dev/null) && return 0
+	return 1
+}
+depset_add(){
+	depset_chk $1 && return 0
+	export PKGDEP="$PKGDEP $1"
+}
