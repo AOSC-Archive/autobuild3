a2201cc108ca3f86040bb1a2f421d414db980761
Icenowy Zheng <icenowylin@gmail.com>
2014-05-17 21:51:12 +0800

    bash library

---
+++ lib/base.sh
@@ -0,0 +1,29 @@
+ABLIBS="|base|"
+bool(){
+	case $1 in
+		0|f|F|false|n|N|no) return 1 ;;
+		1|t|T|true|y|Y|yes) return 0 ;;
+		*) return 1 ;;
+	esac
+}
+abloadlib(){
+	[ -f $AB/lib/$1.sh ] || exit 1
+	. $AB/lib/$1.sh
+	export ABLIBS="${ABLIBS}$1|"
+}
+abrequire(){
+	for i
+	do
+		if echo $ABLIBS | grep "\\|$1\\|" > /dev/null
+		then
+			true
+		elif [ "$i" = "pm" ]
+		then
+			#special
+			. $AB/$ABMPM/lib/pm.sh || exit 1
+			export ABLIBS="${ABLIBS}pm|"
+		else
+			abloadlib $i || exit 1
+		fi
+	done
+}
