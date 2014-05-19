c4224fb5504e55fa5edc658695a50f22a1206b85
Jeff Bai <jeffbaichina@members.fsf.org>
2014-05-18 18:21:33 -0600

    what I was doing was silly...

--- dpkg/lib/pm.sh
+++ dpkg/lib/pm.sh
@@ -3,5 +3,4 @@ abreqexe dpkg dpkg-deb dpkg-query
 pm_whoprov(){
 	dpkg-query -S $1 2> /dev/null | awk '{ print $1 }' | rev | cut -b 2- | rev
 	# This just give a nice list of formatted dependencies.
-	# This file should be renamed as pm_dpkg.sh as we need rpm support also.
 }
