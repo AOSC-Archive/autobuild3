cff030ae3b4d74401206a18d5688521a30b11aa7
Jeff Bai <jeffbaichina@members.fsf.org>
2014-05-18 18:32:40 -0600

    echoes, fixes

--- lib/base.sh
+++ lib/base.sh
@@ -13,8 +13,11 @@ abreqexe(){
 	do
 		if which $i > /dev/null
 		then
+			echo -e "\033[36mLooks like everything needed are here\033[0m"
 			true
 		else
+			echo -e "\033[31mNope, something seemed not being here while you ran autobuild\033[0m"
+			# Needed program which are not here shall be listed below...
 			exit 1
 		fi
 	done
@@ -34,7 +37,8 @@ abrequire(){
 			true
 		elif [ "$i" = "pm" ]
 		then
-			#special
+			# Special
+			# Special what... /etc/autobuild.d/10-pm or whatever to decide the PM used?
 			. $AB/$ABMPM/lib/pm.sh || exit 1
 			export ABLIBS="${ABLIBS}pm|"
 		else
--- lib/elf.sh
+++ lib/elf.sh
@@ -8,5 +8,7 @@ elf_iself()
 elf_strip()
 {
 	elf_iself $1 || return 1
+	# If a library can only be stripped with its debug symbols... 
+	# Just trying to think of a way to seperate those files, like in /{bin,usr/bin,sbin,usr,sbin} or /{lib,usr/lib}
 	strip --strip-debug $1
 }
