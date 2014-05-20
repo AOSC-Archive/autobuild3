2586c7d6aa46ee2194fccbf444776c2d863c442f
Icenowy Zheng <icenowylin@gmail.com>
2014-05-20 17:43:25 +0800

    Towards the final works!

---
+++ scriptlet/01-alternatives.sh
@@ -0,0 +1,17 @@
+alternative(){
+	while [ "x$1" != "x" ]
+	do
+		echo "update-alternatives --install $1 `basename $1` $2 $3 " >> abscripts/postinst
+		echo "update-alternatives --remove `basename $1` $2" >> abscripts/prerm
+		shift
+		shift
+		shift
+	done
+}
+
+if [ -e autobuild/alternatives ]
+then
+	. autobuild/alternatives
+fi
+
+unset alternatives
