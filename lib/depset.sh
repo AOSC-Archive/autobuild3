a43483ce783abf67c29c037c0a65bbe03ad9b83f
Icenowy Zheng <icenowylin@gmail.com>
2014-06-09 19:30:40 +0800

    New depset

--- lib/depset.sh
+++ lib/depset.sh
@@ -2,15 +2,15 @@
 
 depset_chk(){
 	printf "\033[36m>>>\033[0m Querying package dependencies...	 \033[0m\033[35m[INFO]\n\033[0m"
+	(echo $PKGDEP | grep "^[\=><]$1 " > /dev/null) && return 0
+	(echo $PKGDEP | grep " [|\=><]$1\$" > /dev/null) && return 0
+	(echo $PKGDEP | grep " [|\=><]$1 " > /dev/null) && return 0
 	(echo $PKGDEP | grep "^$1 " > /dev/null) && return 0
 	(echo $PKGDEP | grep " $1\$" > /dev/null) && return 0
-	(echo $PKGDEP | grep " $1 " > /dev/null) && return 0
-	(echo $PKGDEP | grep "^$1|" > /dev/null) && return 0
-	(echo $PKGDEP | grep "|$1\$" > /dev/null) && return 0
-	(echo $PKGDEP | grep "|$1 " > /dev/null) && return 0
-	(echo $PKGDEP | grep " $1|" > /dev/null) && return 0
-	(echo $PKGDEP | grep "^$1\$" > /dev/null) && return 0
 	(echo $PKGDEP | grep "|$1|" > /dev/null) && return 0
+	(echo $PKGDEP | grep "|$1 " > /dev/null) && return 0
+	(echo $PKGDEP | grep "|$1\$" > /dev/null) && return 0
+	(echo $PKGDEP | grep " $1|" > /dev/null) && return 0
 	return 1
 	# printf "\033[32m[OK]\n\033[0m"
 }
