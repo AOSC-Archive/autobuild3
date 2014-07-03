028db6867196f42d3305ce473a1948771c072e9f
Jeff Bai <jeffbaichina@members.fsf.org>
2014-07-03 17:10:49 +0800

    Fix

--- build/15-python.sh
+++ build/15-python.sh
@@ -3,7 +3,8 @@ build_python_probe(){
 	[ -f setup.py ]	
 }
 build_python_build(){
-        python2 setup.py install --prefix=/usr --root=`pwd`/abdist
+        python2 setup.py install --prefix=/usr --root=`pwd`/abdist 
+        python2 setup.py clean
         python3 setup.py install --prefix=/usr --root=`pwd`/abdist
 }
 export ABBUILDS="$ABBUILDS python"
