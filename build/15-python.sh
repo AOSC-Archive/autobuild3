47271b8567d20f6855634f98aaf3c5f99526673b
Jeff Bai <jeffbaichina@members.fsf.org>
2014-07-14 18:48:52 +0800

    The legend of a Baka @Arthur200000

--- build/15-python.sh
+++ build/15-python.sh
@@ -1,16 +1,18 @@
-abreqexe python python3
+abreqexe python
 
 build_python_probe(){
         [ -f setup.py ]
 }
 build_python_build(){
         python2 setup.py install --prefix=/usr --root=`pwd`/abdist --optimize=1
-        if bool $NOPYTHON3 || return 1
+        if bool $NOPYTHON3
         then
                 true
         else
                 python2 setup.py clean
                 python3 setup.py install --prefix=/usr --root=`pwd`/abdist --optimize=1
         fi
+        # HACK! unset NOPYTHON3
+        unset NOPYTHON3
 }
 export ABBUILDS="$ABBUILDS python"
