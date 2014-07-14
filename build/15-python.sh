a2fe5a788680e55fe04b9d70f9e8bddc1c95d2cf
Jeff Bai <jeffbaichina@members.fsf.org>
2014-07-14 17:36:04 +0800

    Better implementation.

--- build/15-python.sh
+++ build/15-python.sh
@@ -1,12 +1,16 @@
-abreqexe python
+abreqexe python python3
 
 build_python_probe(){
-	[ -f setup.py ]	
+        [ -f setup.py ]
 }
 build_python_build(){
-        python2 setup.py install --prefix=/usr --root=`pwd`/abdist 
-        bool $NOPYTHON3 || return 0
-        python2 setup.py clean
-        python3 setup.py install --prefix=/usr --root=`pwd`/abdist
+        python2 setup.py install --prefix=/usr --root=`pwd`/abdist --optimize=1
+        if bool $NOPYTHON3 || return 1
+        then
+                true
+        else
+                python2 setup.py clean
+                python3 setup.py install --prefix=/usr --root=`pwd`/abdist --optimize=1
+        fi
 }
 export ABBUILDS="$ABBUILDS python"
