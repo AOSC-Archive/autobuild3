bd040b291090afed83f3c47b6baa6c9e55c76eba
Arthur Wang <arthur200126@gmail.com>
2014-07-16 05:54:49 +0800

    I forgot it..

--- build/10-autotools.sh
+++ build/10-autotools.sh
@@ -4,42 +4,33 @@ export FORCE_UNSAFE_CONFIGURE=1
 SRCDIR=`pwd`
 
 build_autotools_probe(){
-	[ -f configure ] || [ -f autogen.sh ] || [ -f configure.ac ]
+	[ -x configure ] || [ -x autogen.sh ] || [ -x bootstrap] || [ -f configure.ac ] || [ -x $configure ]
 }
 
 build_autotools_build() {
-	[ "$ABSHADOW" != "" ] && export ABSHADOW_AUTOTOOLS=$ABSHADOW
-	if [ ! -e configure ]
-	then
-		if [ -e autogen.sh ]
-		then
-			NOCONFIGURE=1 ./autogen.sh | ablog
-		elif [ -e configure.ac ]
-		then 
-			autoreconf -fi 2>&1 | ablog
+	[ $ABSHADOW ] && export ABSHADOW
+	([ -x bootstrap ] && ! [ -e autogen.sh]) && ln -s bootstrap autogen.sh
+	if [ ! -e configure ]; then
+	  if [ -x autogen.sh ]; then
+	    NOCONFIGURE=1 ./autogen.sh | ablog
+	  elif [ -e configure.ac ]; then 
+			autoreconf -fis 2>&1 | ablog
 		fi
-	else
-		# a configure script is already present
-		true
 	fi
 
-	if bool $ABSHADOW_AUTOTOOLS
+	if bool $ABSHADOW
 	then
 		mkdir -p build 2> /dev/null&&
 		cd build
-	else 
-		true
 	fi
 
-	$SRCDIR/configure $AUTOTOOLS_DEF $AUTOTOOLS_AFTER  | ablog
-	make $ABMK $MAKE_AFTER | ablog
-	make install BUILDROOT=$SRCDIR/abdist DESTDIR=$SRCDIR/abdist $MAKE_AFTER | ablog
-	
-	if bool $ABSHADOW_AUTOTOOLS
+	$SRCDIR/$configure $AUTOTOOLS_DEF $AUTOTOOLS_AFTER  | ablog &&
+	make $ABMK $MAKE_AFTER | ablog &&
+	make install BUILDROOT=$SRCDIR/abdist DESTDIR=$SRCDIR/abdist $MAKE_AFTER | ablog &&
+	_ret=$?
+	if bool $ABSHADOW
 	then 
 		cd ..
-	else
-		true
 	fi
 }
 
