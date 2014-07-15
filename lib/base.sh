30ea0aa4895d4f015fb6f39d94531699385591f4
Jeff Bai <jeffbaichina@members.fsf.org>
2014-07-15 11:22:51 +0800

    Wow such code.

--- lib/base.sh
+++ lib/base.sh
@@ -55,4 +55,4 @@ ablog(){
 
 # Special Source, looks like stacktrace
 .(){ source $* || (printf "  \e[31min $*\e[0m\n"; return 1); }
-recsr(){for sr in "$@"; do . $sr; done}
+recsr(){ for sr in "$@"; do . $sr; done }
