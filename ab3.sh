4d90afa8b410f3b3fafdfdc0841803f20069496a
Arthur Wang <arthur200126@gmail.com>
2014-09-20 16:10:11 +0800

    “自举”

--- ab3.sh
+++ ab3.sh
@@ -3,5 +3,6 @@ export ABSET=/etc/autobuild
 export AB=$(cat $ABSET/prefix 2>/dev/null || echo $(dirname $(dirname $0)))
 export ARCH=$(cat $ABSET/arch 2>/dev/null || uname -m || echo "x86_64")
 [ $AB_DBG ] && set -xv
+[ $AB_SELF ] && AB=$(dirname $0)
 . $AB/lib/base.sh
 . $AB/proc/*.sh
