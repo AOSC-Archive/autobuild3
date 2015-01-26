56459a814538168f211cca1310461c0a8d984874
Jeff Bai <jeffbaichina@members.fsf.org>
2015-01-25 17:53:26 -0700

    --force can break stuff

--- proc/99-install.sh
+++ proc/99-install.sh
@@ -4,5 +4,5 @@ do
 	. $AB/$i/install
 done
 else dpkg -i $PKGNAME.deb || aberr "DPKG installation failed."
-rpm -Uvh --force /root/rpmbuild/RPMS/x86_64/$PKGNAME-$PKGVER-$PKGREL*.rpm || aberr "RPM installation failed."
+rpm -Uvh /root/rpmbuild/RPMS/x86_64/$PKGNAME-$PKGVER-$PKGREL*.rpm || aberr "RPM installation failed."
 fi
