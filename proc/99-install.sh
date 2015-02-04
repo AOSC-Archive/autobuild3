8ec49e76933426bca7fa5809bf18f420ecb5769c
Jeff Bai <jeffbaichina@members.fsf.org>
2015-02-03 19:38:45 -0700

    That was silly...

--- proc/99-install.sh
+++ proc/99-install.sh
@@ -1,8 +1,6 @@
 if bool $ABINSTALL
 then for i in $ABMPM $ABAPMS
 do
-	. $AB/$i/install
+	. $AB/$i/lib/install.sh
 done
-else dpkg -i $PKGNAME.deb || aberr "DPKG installation failed."
-# rpm -Uvh /root/rpmbuild/RPMS/x86_64/$PKGNAME-$PKGVER-$PKGREL*.rpm || aberr "RPM installation failed."
 fi
