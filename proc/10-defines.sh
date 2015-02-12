f4f6b8bb63b6094cde2b34d4d453affbab2b7cbd
Mingcong Bai <jeffbaichina@members.fsf.org>
2015-02-11 18:11:33 -0700

    boom... I am the beary thing

--- proc/10-defines.sh
+++ proc/10-defines.sh
@@ -1,11 +1,11 @@
 export SRCDIR="$PWD"
 export PKGDIR="$PWD/abdist"
 
-[ -e autobuild/defines ] && { . autobuild/defines || abwarn "autobuild/defines returned a non-zero value." 
-} || aberr "autobuild/defines not found."
-
 # Autobuild settings
 recsr $AB/etc/defaults/*
+
+[ -e autobuild/defines ] && { . autobuild/defines || abwarn "autobuild/defines returned a non-zero value." 
+} || aberr "autobuild/defines not found."
 
 alias make='make $ABMK' # aliases aren't global, take care
 
