e9e7159806f82654645124c532ccc2d1fcca1716
Mingcong Bai <jeffbaichina@members.fsf.org>
2015-02-18 09:13:51 -0700

    not needed

--- proc/65-canonical-ownership.sh
+++
@@ -1,6 +0,0 @@
-abinfo "Creating en-listed items for canonical-ownership"
-
-mkdir -p $PKGDIR/var/ab/canonical-ownership
-
-for enlist in `find abdist/`; do 
-    stat -c "%n %a %u %g" $enlist; done | cut -c 7- > $PKGDIR/var/ab/canonical-ownership/$PKGNAME
