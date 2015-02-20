e0c9a5ff3c925f12f0e2f07771862b6cd0ef1bc4
Jeff Bai <jeffbaichina@members.fsf.org>
2015-02-20 10:18:35 -0700

    Clang support

--- proc/11-flags.sh
+++ proc/11-flags.sh
@@ -1,11 +1,25 @@
-if bool $NOLTO; then
-    export CPPFLAGS="$CPPFLAGS_NOLTO" 
-    export CFLAGS="$CFLAGS_NOLTO"
-    export CXXFLAGS="$CXXFLAGS_NOLTO"
-    export LDFLAGS="$LDFLAGS_NOLTO"
+if bool $USECLANG; then
+    if bool $NOLTO; then
+        export CPPFLAGS="$CPPFLAGS_CLANG_NOLTO" 
+        export CFLAGS="$CFLAGS_CLANG_NOLTO"
+        export CXXFLAGS="$CXXFLAGS_CLANG_NOLTO"
+        export LDFLAGS="$LDFLAGS_CLANG_NOLTO"
+    else
+        export CPPFLAGS="$CPPFLAGS_CLANG_LTO"
+        export CFLAGS="$CFLAGS_CLANG_LTO"
+        export CXXFLAGS="$CXXFLAGS_CLANG_LTO"
+        export LDFLAGS="$LDFLAGS_CLANGLTO"
+    fi
 else
-    export CPPFLAGS="$CPPFLAGS_LTO"
-    export CFLAGS="$CFLAGS_LTO"
-    export CXXFLAGS="$CXXFLAGS_LTO"
-    export LDFLAGS="$LDFLAGS_LTO"
+    if bool $NOLTO; then
+        export CPPFLAGS="$CPPFLAGS_NOLTO" 
+        export CFLAGS="$CFLAGS_NOLTO"
+        export CXXFLAGS="$CXXFLAGS_NOLTO"
+        export LDFLAGS="$LDFLAGS_NOLTO"
+    else
+        export CPPFLAGS="$CPPFLAGS_LTO"
+        export CFLAGS="$CFLAGS_LTO"
+        export CXXFLAGS="$CXXFLAGS_LTO"
+        export LDFLAGS="$LDFLAGS_LTO"
+    fi
 fi
