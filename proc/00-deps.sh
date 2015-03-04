ed06983f14a72991650b8ecea457e6704b171e18
Jeff Bai <jeffbaichina@members.fsf.org>
2015-03-04 14:29:12 -0700

    Update 00-deps.sh

--- proc/00-deps.sh
+++ proc/00-deps.sh
@@ -1,5 +1,6 @@
 abrequire pm
 pm_checkdep $BUILDDEP $PKGDEP || (
+abinfo "Build or runtime dependencies not satisfied, now fetching needed packages."
 pm_repoupdate
 pm_repoinstall $BUILDDEP $PKGDEP
 )
