b66c11b2b0d8b4e7b91d94941b54def16d8257ca
Jeff Bai <jeffbaichina@members.fsf.org>
2015-03-04 14:27:24 -0700

    RPM merge

--- rpm/lib/pm.sh
+++ rpm/lib/pm.sh
@@ -4,5 +4,10 @@ pm_getver(){
 	rpm -qi $1 | grep 'Version' | awk '{ print $3 }' 2>/dev/null
 }
 
+if ! rpm -qa $BUILDDEP $PKGDEP; then
+    zypper ref -f
+    zypper install $BUILDDEP $PKGDEP
+fi
+
 # pm_whoprov
 # Just for testing of output.
