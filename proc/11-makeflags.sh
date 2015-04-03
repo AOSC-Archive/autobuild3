fcdd0b7a38981136dbd18dd1c0ee5185d5c780db
Mingcong Bai <jeffbaichina@members.fsf.org>
2015-04-03 17:44:43 -0600

    more messages

--- proc/11-makeflags.sh
+++ proc/11-makeflags.sh
@@ -1,6 +1,9 @@
 # MAKEFLAGS usage. ABMK as supplement.
 if bool $NOPARALLEL; then
+    abwarn "Parallel build DISABLED, get a cup of coffee, now!"
+    sleep 2
     export MAKEFLAGS=
 else
+    abinfo "Parallel build ENABLED"
     export MAKEFLAGS="-j $(echo $(lscpu | grep ^CPU\(s\) | awk '{ print $2 }')*2+1 | bc)"
 fi
