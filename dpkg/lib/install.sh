49984ca8a0ca548f0fae8416f2f3025df8f6c162
Mingcong Bai <jeffbaichina@members.fsf.org>
2015-04-03 17:25:03 -0600

    carelessness

--- dpkg/lib/install.sh
+++ dpkg/lib/install.sh
@@ -1,1 +1,1 @@
-dpkg -i $PKGNAME.deb
+dpkg -i ${PKGNAME}_${PKGVER}-${PKGREL}_${ARCH}.deb
--- proc/99-install.sh
+++ proc/99-install.sh
@@ -1,8 +1,8 @@
 # ABINSTALL no longer will be fed with boolean.
 # Put in PMs instead.
 
-then for i in $ABINSTALL
+for i in $ABINSTALL
 do
 	. $AB/$i/lib/install.sh
 done
-fi
+
