153fa238bfc4cc6baae7f8af104ed16c80aef212
Icenowy <icenowy@outlook.com>
2015-05-11 13:54:14 +0800

    11-compiler.sh cross sensible

--- proc/11-compiler.sh
+++ proc/11-compiler.sh
@@ -1,15 +1,18 @@
 # Compiler configuration.
 
-if [ -e /usr/bin/gcc-multilib-wrapper ]; then
-	if bool $USECLANG; then
-		export CC=/usr/bin/clang CXX=/usr/bin/clang++
+if [ "$CROSS" = "" ]; then
+	
+	if [ -e /usr/bin/gcc-multilib-wrapper ]; then
+		if bool $USECLANG; then
+			export CC=/usr/bin/clang CXX=/usr/bin/clang++
+		else
+			export CC=/usr/bin/gcc-multilib-wrapper CXX=/usr/bin/g++-multilib-wrapper
+		fi
 	else
-		export CC=/usr/bin/gcc-multilib-wrapper CXX=/usr/bin/g++-multilib-wrapper
-	fi
-else
-	if bool $USECLANG; then
-		export CC=/usr/bin/clang CXX=/usr/bin/clang++
-	else
-		export CC=/usr/bin/gcc CXX=/usr/bin/g++
+		if bool $USECLANG; then
+			export CC=/usr/bin/clang CXX=/usr/bin/clang++
+		else
+			export CC=/usr/bin/gcc CXX=/usr/bin/g++
+		fi
 	fi
 fi
