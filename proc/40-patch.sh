d96ee84d6a790281c3e7e0bcbefb296d4969eda4
Jeff Bai <jeffbaichina@members.fsf.org>
2015-05-12 09:28:11 -0600

    automate autotools reconfiguration

--- proc/40-patch.sh
+++ proc/40-patch.sh
@@ -6,6 +6,13 @@ then
 	if [ -f `arch_filefind patch` ]
 	then
 		. `arch_filefind patch`
+		if [ ${ABTYPE} = "autotools" ]; then
+			if [ -x $SRC/autogen.sh ]; then
+				NOCONFIGURE=1 ./autogen.sh
+			else
+				autoreconf -fi
+			fi
+		fi
 		touch .patch
 	elif [ -f autobuild/patches/series ]
 	then
@@ -13,12 +20,26 @@ then
 			patch -Np1 -i autobuild/patches/$i
 			# Some patches are not -Np1 prefixed, keep in mind.
 		done
+		if [ ${ABTYPE} = "autotools" ]; then
+			if [ -x $SRC/autogen.sh ]; then
+				NOCONFIGURE=1 ./autogen.sh
+			else
+				autoreconf -fi
+			fi
+		fi
 		touch .patch
 	elif [ -d autobuild/patches ]
 	then
 		for i in autobuild/patches/*.{patch,diff}; do
 			patch -Np1 -i $i
 		done
+		if [ ${ABTYPE} = "autotools" ]; then
+			if [ -x $SRC/autogen.sh ]; then
+				NOCONFIGURE=1 ./autogen.sh
+			else
+				autoreconf -fi
+			fi
+		fi
 		touch .patch
 	fi
 
