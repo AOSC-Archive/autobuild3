c82cce39cd212584c650668a5625efa5b0350706
Jeff Bai <jeffbaichina@members.fsf.org>
2015-06-16 21:38:15 +0800

    Add trailing space.

--- arch/armel.sh
+++ arch/armel.sh
@@ -5,7 +5,7 @@ fi
 if bool loading_host; then
 	HOST=armv7a-hardfloat-linux-gnueabi
 	[ "$CROSS" ] && HOSTTOOLPREFIX=/opt/abcross/armel/bin/armv7a-hardfloat-linux-gnueabi
-	[ "$CROSS" ] && LDFLAGS_COMMON_ARCH='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/armel/usr/lib -L/var/ab/cross-root/armel/usr/lib'
-	CFLAGS_COMMON_ARCH='-march=armv7-a -mtune=cortex-a7 -mfpu=neon -mfloat-abi=hard'
+	[ "$CROSS" ] && LDFLAGS_COMMON_ARCH='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/armel/usr/lib -L/var/ab/cross-root/armel/usr/lib '
+	CFLAGS_COMMON_ARCH='-march=armv7-a -mtune=cortex-a7 -mfpu=neon -mfloat-abi=hard '
 fi
 
