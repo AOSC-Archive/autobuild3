c12bb7218506858b36740c428381b12716946a54
Mingye Wang <arthur200126@gmail.com>
2015-06-18 00:20:55 +0800

    depset: adopt new pm syntax.

--- lib/depset.sh
+++ lib/depset.sh
@@ -1,11 +1,8 @@
-abreqexe grep
-
 # Checks if the current string contains the dep argv[1]
-# old eregex: grep -E "(^| )[\=><]?$1( |\$)" <<< "$PKGDEP"
+# Pollutes: BASH_REMATCH, consider rewrite in extglob.
 depset_contains(){
-	grep -E "(^| )[\=><]?$1( |\$)" <<< "$PKGDEP"
+	[[ "$PKGDEP" =~ (^| )$1(_|[<>=]=[^ ]*)?( |$) ]]
 }
-
 depset_add(){
 	depset_contains $1 && return 0
 	PKGDEP+=" $1"
