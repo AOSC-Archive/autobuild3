ca329d92a8fa731c22e4a4972f76687a3aff8586
Mingye Wang <arthur200126@gmail.com>
2015-06-18 10:44:29 +0800

    python: delayed probe || echo :)

--- build/15-python.sh
+++ build/15-python.sh
@@ -1,17 +1,16 @@
 abreqexe python
+abtryexe python2 python3
 
 build_python_probe(){
-	[ -f setup.py ] || return 1
-	if bool $NOPYTHON2 && ! bool $NOPYTHON3; then
-		PYTHON="$(which python2 || which python)"
-	else
-		PYTHON="$(which python3)"
-	fi
+	[ -f setup.py ]
 }
 
 build_python_build(){
-	$PYTHON setup.py install $MAKE_AFTER --prefix=/usr --root=$PKGDIR --optimize=1 || return $?
-	bool $NOPYTHONCLEAN && $PYTHON setup.py clean || true
+	for PYTHON in "$(bool NOPYTHON2 || which python2 || which python || echo :)" \
+	"$(bool NOPYTHON3 || which python3 || echo :)"; do
+		"$PYTHON" setup.py install $MAKE_AFTER --prefix=/usr --root="$PKGDIR" --optimize=1 || return $?
+		bool $NOPYTHONCLEAN || "$PYTHON" setup.py clean || true
+	fi
 }
 
 export ABBUILDS="$ABBUILDS python"
