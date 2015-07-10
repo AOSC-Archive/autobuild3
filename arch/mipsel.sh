54ee4f0a0ebc1aae42794e7c8a9e716a7d443919
Mingcong Bai <jeffbaichina@members.fsf.org>
2015-07-10 10:01:51 +0800

    ls2f: tune for it

--- arch/mipsel.sh
+++ arch/mipsel.sh
@@ -1,2 +1,2 @@
 # FIXME: Loongson workaround added as suggested by biergaizi, do this break anything?
-CFLAGS_COMMON_ARCH='-march=mips3 -mtune=mips3 -mabi=n32 -Wa,-mfix-loongson2f-nop '
+CFLAGS_COMMON_ARCH='-march=mips3 -mtune=loongson2f -mabi=n32 -Wa,-mfix-loongson2f-nop '
