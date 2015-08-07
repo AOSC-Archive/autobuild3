5a7e10c6ab3554b89cda7c100f1505cb95b87dd8
Mingye Wang <arthur200126@163.com>
2015-08-07 15:11:21 +0000

    makeflags: move back to ABMK

--- proc/11-makeflags.sh
+++ proc/11-makeflags.sh
@@ -1,9 +1,7 @@
-# MAKEFLAGS usage. ABMK as supplement.
+# Still, let's use ABMK. MAKEFLAGS should be in defines.
 if bool $NOPARALLEL; then
 	abwarn "Parallel build DISABLED, get a cup of coffee, now!"
-	sleep 2
-	export MAKEFLAGS=
 else
 	abinfo "Parallel build ENABLED"
-	export MAKEFLAGS="-j $(( $(nproc) * 2 + 1))"
+	ABMK+=" -j$(( $(nproc) * 2 + 1)) "
 fi
