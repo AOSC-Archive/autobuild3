5de9897dd53e3310a33bb07f906c325dd933ff3a
Mingcong Bai <jeffbaichina@members.fsf.org>
2015-08-20 16:18:31 -0600

    arch: factor hardening-related flags to config

--- arch/_common.sh
+++ arch/_common.sh
@@ -1,8 +1,6 @@
 # Common archdef for all ab arches, unsettable so the flags can be erased.
 AB_FLAGS_TYPES='_OPTI _ARCH _WEIRD '
 AB_FLAGS_FEATURES='LTO PERMISSIVE '
-AB_FLAGS_PIC=1
-AB_FLAGS_PIE=1
 # Naming, sadly. PORT_NOTICE!
 ARCH_TARGET[amd64]=x86_64-unknown-linux-gnu
 ARCH_TARGET[armel]=armv7a-hardfloat-linux-gnueabi
@@ -11,7 +9,7 @@ ARCH_TARGET[mipsel]=mips64el-linux-gnuabin32
 ARCH_RPM[amd64]=x86_64
 ARCH_RPM[armel]=armfhp # real story: we are using neon.
 # C Compiler Flags.
-CFLAGS_COMMON='-pipe -fstack-protector-strong --param=ssp-buffer-size=4 -Wno-error '
+CFLAGS_COMMON='-pipe -Wno-error '
 CFLAGS_COMMON_OPTI='-fomit-frame-pointer -O2 '
 CFLAGS_COMMON_OPTI_LTO='-flto '
 CFLAGS_COMMON_DEBUG='-O '	# not that frequently used since autotools know it.
@@ -35,12 +33,16 @@ OBJCXXFLAGS_COMMON_PERMISSIVE="-fpermissive "
 # LDFLAGS writing helpers:
 ld_arg(){ echo -n -Wl; local arg ABCOMMA=,; for arg; do abmkcomma; echo -n "$arg"; done; }
 ld_path(){ local path=$(arch_lib "$@"); ld_arg "$path"; echo -n " -L$path"; }
-LDFLAGS_COMMON='-Wl,-O1,--sort-common,--as-needed,-z,relro -Wl,-z,now '
+LDFLAGS_COMMON='-Wl,-O1,--sort-common,--as-needed '
 #LDFLAGS_COMMON_OPTI='-Wl,--relax '	# on some arches this interfere with debugging, therefore put into OPTI.
 # temporarily disabled because this breaks core-devel/glibc build (-r cannot be used together with --relax).
 # investigation advised.
 LDFLAGS_COMMON_OPTI_LTO='-flto -fuse-linker-plugin '
 LDFLAGS_COMMON_OPTI_NOLTO='-fno-lto -fno-use-linker-plugin '
 LDFLAGS_COMMON_CROSS_BASE="-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link $(ld_path) "
+# Switches for hardening related flags.
 if ((AB_FLAGS_PIC)); then LDFLAGS_COMMON+='-fPIC ' CFLAGS_COMMON+='-fPIC '; fi 
 if ((AB_FLAGS_PIE)); then LDFLAGS_COMMON+='-fPIE -pie ' CFLAGS_COMMON+='-fPIE -pie '; fi
+if ((AB_FLAGS_SSP)); then CFLAGS_COMMON+='-fstack-protector-strong --param=ssp-buffer-size=4 '; fi
+if ((AB_FLAGS_RRO)); then LDFLAGS_COMMON+='-Wl,-z,relro '; fi
+if ((AB_FLAGS_NOW)); then LDFLAGS_COMMON+='-Wl,-z,now '; fi
