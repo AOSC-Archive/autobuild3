bb26447514100f4b1506f716228af875924323b5
Jeff Bai <jeffbai@aosc.xyz>
2015-08-30 17:22:44 -0600

    mipsel: sysv hashing style is the one and only supported

--- arch/mipsel.sh
+++ arch/mipsel.sh
@@ -1,2 +1,3 @@
 # FIXME: Loongson workaround added as suggested by biergaizi, do this break anything?
 CFLAGS_COMMON_ARCH='-march=mips3 -mtune=loongson2f -mabi=n32 -Wa,-mfix-loongson2f-nop '
+LDFLAGS_COMMON_ARCH='-Wl,--hash-style=sysv '
