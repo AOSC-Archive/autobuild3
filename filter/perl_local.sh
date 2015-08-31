a561b5ffb56931dcf287eeef2d685df33403ce4c
Jeff Bai <jeffbai@aosc.xyz>
2015-08-31 17:46:58 -0600

    perl_local.sh: use abinfo.

--- filter/perl_local.sh
+++ filter/perl_local.sh
@@ -1,5 +1,6 @@
 filter_perllocal(){
-	rm -rfv usr/lib/perl5/5.20.0/x86_64-linux-thread-multi/perllocal.pod
+	abinfo "Removed perllocal.pod."
+	rm -rf usr/lib/perl5/5.20.0/x86_64-linux-thread-multi/perllocal.pod
 }
 
 export ABFILTERS="$ABFILTERS perllocal"
