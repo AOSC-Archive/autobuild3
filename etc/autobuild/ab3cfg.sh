424df2ff680a020117eb0139d3c2c68004e9cad8
Mingye Wang <arthur200126@gmail.com>
2015-10-25 07:08:50 -0400

    ab3cfg: accept env MTER, fix #73

--- etc/autobuild/ab3cfg.sh
+++ etc/autobuild/ab3cfg.sh
@@ -3,4 +3,4 @@
 # See a list of options in ab3_defcfg.sh.
 ABMPM=dpkg  # Your main PM
 ABAPMS=rpm  # Other PMs
-MTER="AOSC Buildbot <aosc@members.fsf.org>" # And your name and email
+: ${MTER='AOSC Buildbot <aosc@members.fsf.org>'} # And your name and email
