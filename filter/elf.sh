60b8db3dbe028e61aa1d22eb2284ce21f695beef
Mingcong Bai <jeffbai@aosc.xyz>
2015-11-10 08:39:35 -0700

    elf: actually fixing the issue

--- filter/elf.sh
+++ filter/elf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##filter/elf.sh: ELF-related filters
 ##@copyright GPL-2.0+
-abrequire elf
+abrequire fileenum elf
 
 recsr $AB/filter/elf/*.sh
 
@@ -19,10 +19,10 @@ filter_elf(){
 	local _elf_f _elf_cmd
 	for _elf_f in $ABELFFILTER; do _elf_cmd=filter_elf_${_elf_f}_pre; ! _which $_elf_cmd &>/dev/null || $_elf_cmd || abwarn "$(argprint $_elf_cmd "$@"): $?"; done
 	set_opt nullglob # Force existing(nullglob) directories(/).
-	for i in "$PKGDIR"/{[o]pt/*/,[u]sr/,}{[l]ib{,64,exec},{s,}[b]in}/**
+	for i in "$PKGDIR"/{[o]pt/*/,[u]sr/,}{[l]ib{,64,exec},{s,}[b]in}/
 	do
-                cd "$i"
-                fileenum "filter_elf__process {}"
+		cd "$i"
+		fileenum "filter_elf__process {}"
 	done
 	cd "$PKGDIR"
 	for _elf_f in $ABELFFILTER; do _elf_cmd=filter_elf_${_elf_f}_post; ! _which $_elf_cmd &>/dev/null || $_elf_cmd || abwarn "$(argprint $_elf_cmd "$@"): $?"; done
