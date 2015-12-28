0052b2be72ab0cae7a4e7ed5b831dc7e4d44b138
Mingcong Bai <jeffbai@aosc.xyz>
2015-12-27 20:55:50 -0700

    arch: use vfpv3-d16 as default fpu for armel

--- arch/armel.sh
+++ arch/armel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ##arch/armel.sh: Build definitions for armel.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-march=armv7-a -mtune=cortex-a7 -mfpu=neon -mfloat-abi=hard -funsafe-math-optimizations '
+CFLAGS_COMMON_ARCH='-march=armv7-a -mtune=cortex-a7 -mfloat-abi=hard -mfpu=vfpv3-d16 '
 LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/armel/usr/lib -L/var/ab/cross-root/armel/usr/lib '
