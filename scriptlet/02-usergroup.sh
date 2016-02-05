858aff384f19cfc006a0a4f4e6e52b911e9d250d
Mingye Wang <arthur200126@gmail.com>
2016-02-05 01:53:42 -0500

    usermod can be done in one command

--- scriptlet/02-usergroup.sh
+++ scriptlet/02-usergroup.sh
@@ -4,9 +4,10 @@
 # user LOGIN UID GID HOME COMMENT SHELL [GROUPS..]
 user(){
 	echo "grep -q '^$1:' /etc/passwd || useradd $(argprint -u "$2" -g "$3" -d "$4"  -c "$5" -s "$6" "$1" -m)" >> abscripts/preinst
-	local N=$1
+	local N=$1 IFS=,
 	shift 6
-	for i; do echo "usermod -a -G $(argprint "$i" "$N")" >> abscripts/preinst; done
+	# $*: argv[1...].join(IFS[0])
+	echo "usermod -a '-G${*//\'/"'\''"}' '${N//\'/"'\''"}'" >> abscripts/preinst
 }
 
 # group NAME GID
