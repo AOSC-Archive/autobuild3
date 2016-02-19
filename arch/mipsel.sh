370837173132f8f01975c1740ddbf838f7afb0f2
Mingcong Bai <jeffbai@aosc.xyz>
2016-02-18 21:52:39 -0700

    arch: fix wrong abi definition for mipsel

--- arch/mipsel.sh
+++ arch/mipsel.sh
@@ -2,5 +2,5 @@
 ##arch/mipsel.sh: Build definitions for mipsel (loongson 2f best).
 ##@copyright GPL-2.0+
 # FIXME: Loongson workaround added as suggested by biergaizi, do this break anything?
-CFLAGS_COMMON_ARCH='-march=mips3 -mtune=loongson2f -mabi=o32 -Wa,-mfix-loongson2f-nop '
+CFLAGS_COMMON_ARCH='-march=mips3 -mtune=loongson2f -mabi=32 -Wa,-mfix-loongson2f-nop '
 LDFLAGS_COMMON_ARCH='-Wl,--hash-style=sysv '
