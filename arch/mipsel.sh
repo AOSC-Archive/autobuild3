ff2778bd267721c8dc548f0c944def1b18ef502e
Mingcong Bai <jeffbai@aosc.xyz>
2016-02-21 10:28:21 -0700

    arch: use no sysv for mipsel

--- arch/mipsel.sh
+++ arch/mipsel.sh
@@ -3,4 +3,4 @@
 ##@copyright GPL-2.0+
 # FIXME: Loongson workaround added as suggested by biergaizi, do this break anything?
 CFLAGS_COMMON_ARCH='-march=mips2 -mtune=loongson2f -mabi=32 -Wa,-mfix-loongson2f-nop '
-LDFLAGS_COMMON_ARCH='-Wl,--hash-style=sysv '
+#LDFLAGS_COMMON_ARCH='-Wl,--hash-style=sysv '
