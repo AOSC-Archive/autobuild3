1dc01040b9a669997be3953501953e3611b117ff
Mingye Wang <arthur200126@gmail.com>
2016-03-07 03:20:08 -0500

    libpm: CR.

--- lib/pm.sh
+++ lib/pm.sh
@@ -8,31 +8,32 @@ _ab_pm='' # package manager spec variables
 # genver <pkgspec> -> pkgname[<ver_s><op>verstr<ver_e>]
 _ab_pm+="OP_{EQ,LE,GE} VER_{S,E} "
 pm_genver(){
-	local store IFS ver name
-	: ${OP_EQ== } ${OP_LE=<= } ${OP_GE=>= } ${VER_S= (} ${VER_E=)}
+	local store IFS ver name       # IFS is also used foo holding OP.
+	: "${OP_EQ== } ${OP_LE=<= } ${OP_GE=>= } ${VER_S= (} ${VER_E=)}"
 	if ((VER_NONE_ALL)); then			# name-only
 		name="${1/%_}"
 		echo "${name/[<>=]=}"; return
 	elif [[ "$1" =~ [\<\>=]= ]]; then		# nameOP[ver] -> name OP_ ver
-		IFS="$BASH_REMATCH"	# split string using OP
-		store=($1)
+		IFS="$BASH_REMATCH"	# split string using each char in OP
+		read -ra store <<< "$1" 
 		name=${store[0]} ver=${store[2]}	# constexpr store[${#IFS}]
 		IFS=${IFS/==/$OP_EQ}	# translate to package manager notation
 		IFS=${IFS/<=/$OP_LE}
 		IFS=${IFS/>=/$OP_GE}
 	elif ((VER_NONE)) || [[ "$1" =~ _$ ]]; then	# name{,_} -> name (e.g. conflicts, ..)
 		echo -n "${1%_}"; return;
-	else name=$1 IFS="$OP_GE"			# name -> name OP_GE getver
+	else
+		name=$1 IFS="$OP_GE"			# name -> name OP_GE getver
 	fi
 	echo -n "$name$VER_S$IFS${ver=$(pm_getver "$1")}$VER_E"
 }
 # depcom: Turns "$@" into a comma seperated list of deps.
 _ab_pm+="PM_{ALT,DELIM,COMMA} VER_NONE "
 pm_depcom(){
-	: ${PM_ALT=1} ${PM_DELIM= | } ${PM_COMMA=, }
-	local IFS=\| dep pkg i cnt=0	# cnt_depcom: dep comma pos
+	: "${PM_ALT=1} ${PM_DELIM= | } ${PM_COMMA=, }"
+	local IFS='|' dep pkg i cnt=0	# cnt_depcom: dep comma pos
 	for i; do
-		dep=($i)
+		read -ra dep <<< "$i"
 		ABCOMMA="$PM_COMMA" abmkcomma
 		pm_deparse
 	done
@@ -43,11 +44,11 @@ pm_deparse(){
 	if (( !PM_ALT && ${#dep[@]}>1 )); then
 		abwarn "$ABPM doesn't support dependency alternative listing.\n\t" \
 			"Using first dependency: $dep."
-		pm_genver ${dep[0]}; return;
+		pm_genver "${dep[0]}"; return;
 	fi
-	for pkg in ${dep[@]}; do
+	for pkg in "${dep[@]}"; do
 		ABCOMMA="$PM_DELIM" abmkcomma
-		pm_genver $pkg
+		pm_genver "$pkg"
 	done
 }
 # flattens "$@" to a simple list of package names, just like an otokonoko's..
