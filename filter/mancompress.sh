f7d5d2334d2f8b6223138688854267bc34603061
Mingye Wang <arthur200126@gmail.com>
2016-03-07 03:44:19 -0500

    fltr/mancomp: guess what we have symlink handling
    close #47

--- filter/mancompress.sh
+++ filter/mancompress.sh
@@ -3,5 +3,18 @@
 ##@copyright GPL-2.0+
 ((ABMANCOMPRESS)) || return
 set_opt globstar
-gzip -9 "$PKGDIR"/usr/share/man/**/*.[0-9nl]
+__mancomp_todo=()
+for i in "$PKGDIR"/usr/share/man/**/*.[0-9nl]; do
+  if [[ -f $i ]]; then
+    __mancomp_todo+=("$i")
+  elif [[ -L $i ]]; then
+    __mancomp_lnk=$(readlink  -- "$i")
+    rm "$i"
+    ln -sf "$__mancomp_lnk".gz "$i"
+  else
+    abwarn "abmancomp WTF for ${i#"$PKGDIR"}"
+  fi
+done
+gzip -9 -- "${__to_comp[@]}"
+unset __mancomp_todo __mancomp_lnk
 rec_opt globstar
