613139a5dc6be1e2f9307c0e5a22c66653ff5460
Mingcong Bai <jeffbai@aosc.xyz>
2016-03-06 21:22:18 -0700

    filter/perl_local: use globstar style match pattern

--- filter/perl_local.sh
+++ filter/perl_local.sh
@@ -3,7 +3,7 @@
 ##@copyright GPL-2.0+
 filter_perllocal(){
 	abinfo "Removed perllocal.pod."
-	rm -rf usr/lib/perl5/5.20.0/x86_64-linux-thread-multi/perllocal.pod
+	rm -f "$PKGDIR"/usr/lib/perl5/**/perllocal.pod
 }
 
 export ABFILTERS="$ABFILTERS perllocal"
