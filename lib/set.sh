323051e0167b4e6fd7c064cfb866aca1dc78a2ca
Mingye Wang <arthur200126@gmail.com>
2016-03-10 10:50:01 -0500

    set: intersection

--- lib/set.sh
+++ lib/set.sh
@@ -8,7 +8,7 @@ abset_new(){ declare -gA "$1"; }
 # create a local set
 alias abset_loc='declare -A'
 # helper
-alias _abset_ab='declare -n __a="$1" __b="$2"'
+alias _abset_AB='declare -n __A="$1" __B="$2"'
 
 ## PRIMITIVES
 # add opts to set (won't complain if already there)
@@ -16,7 +16,7 @@ alias _abset_ab='declare -n __a="$1" __b="$2"'
 abset_add(){ declare -n __set="$1"; local __idx; for __idx in "${@:1}"; do __set["$__idx"]=1; done; }
 # del opts from set (won't complain if not found)
 # also handy for `difference` (-)
-abset_del(){ local __a=("${@:1}"); __a=("${a[@]/#/$1\[}"); unset "${a[@]/%/\]}"; }
+abset_del(){ local __A=("${@:1}"); __A=("${a[@]/#/$1\[}"); unset "${a[@]/%/\]}"; }
 
 ## Set-ish OP
 ## Convention: returned values stored in _setRet{set}.
@@ -26,35 +26,39 @@ abset_or(){ :; }
 abset_dif(){ :; }
 # symdiff (^)     -- TODO: chained ver
 abset_xor(){
-	_abset_ab
+	_abset_AB
 	abset_new _setRet
-	abset_loc __c
-	abset_add __c "${!__a[@]}"
-	abset_add _setRet "${!__b[@]}"
-	abset_del __c "${!__b[@]}"
-	abset_del _setRet "${!__a[@]}"
+	abset_loc __C
+	abset_add __C "${!__A[@]}"
+	abset_add _setRet "${!__B[@]}"
+	abset_del __C "${!__B[@]}"
+	abset_del _setRet "${!__A[@]}"
 	abset_add _setRet "${!c[@]}"
 }
 # intersection (&) -- TODO: ditto.
 abset_and(){
-	_abset_ab
-	
+	abset_loc __C
+	abset_add __C "${!__A[@]}" "${!__B[@]}"
+	abset_xor "$1" "$2"
+	abset_del __C "${!_setRet[@]}"
+	abset_new _setRet
+	abset_add _setRet "${!__C[@]}"
 }
 
 ## CMP op
 # a == b?
 abset_eq(){
-	_abset_ab
-	[[ "${!__a[@]}" == "${!__b[@]}" ]]
+	_abset_AB
+	[[ "${!__A[@]}" == "${!__B[@]}" ]]
 }
 # a != b?
-abset_ne(){ _abset_ab; [[ "${!__a[@]}" != "${!__b[@]}" ]]; }
+abset_ne(){ _abset_AB; [[ "${!__A[@]}" != "${!__B[@]}" ]]; }
 # a <= b? (subset)
 abset_le(){
-	_abset_ab
-	abset_loc __c
-	abset_add __c "${!__a[@]}" "${!__b[@]}"
-	[[ "${!__b[@]}" == "${!__c[@]}" ]]
+	_abset_AB
+	abset_loc __C
+	abset_add __C "${!__A[@]}" "${!__B[@]}"
+	[[ "${!__B[@]}" == "${!__C[@]}" ]]
 }
 # a >= b?
 abset_ge(){ abset_le "$2" "$1"; }
@@ -65,4 +69,4 @@ abset_gt(){ abset_lt "$2" "$1"; }
 
 ## MISC
 # dump a set into an array (useless)
-abset_arr(){ declare -n __set="$1" __arr="$2"; __arr=("${!__set[@]}"); }
+abset_arr(){ declare -n __set="$1" __Arr="$2"; __Arr=("${!__set[@]}"); }
