c14ec3d9ca40530b1a2af7bc7c596e9b325469ed
Mingcong Bai <jeffbai@aosc.xyz>
2016-03-11 23:43:11 -0700

    ab3_defcfg: remove --disable-debug
    This can break GLib+GTK+ programs! More to come on aosc-os-abbs.

--- etc/autobuild/ab3_defcfg.sh
+++ etc/autobuild/ab3_defcfg.sh
@@ -83,7 +83,7 @@ QT5BIN="/usr/lib/qt5/bin"
 ##OS basic configuration flags
 AUTOTOOLS_DEF="--prefix=$PREFIX --sysconfdir=$SYSCONF --localstatedir=$STATDIR \
 --libdir=$LIBDIR --bindir=$BINDIR --sbindir=$BINDIR --disable-option-checking \
---disable-static --disable-debug --enable-shared"
+--disable-static --enable-shared"
 CMAKE_DEF="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_LIBDIR=lib -DLIB_INSTALL_DIR=lib -DSYSCONF_INSTALL_DIR=$SYSCONF \
 -DCMAKE_SKIP_RPATH=ON"
