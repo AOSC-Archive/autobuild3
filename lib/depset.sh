e1c2044970857cd0c10380e89caf10205ac3fee4
Mingye Wang (Arthur2e5) <arthur200126@gmail.com>
2016-03-15 15:21:02 -0400

    libdepset: data

--- lib/depset.sh
+++ lib/depset.sh
@@ -4,11 +4,15 @@
 # Checks if the current string contains the dep argv[1]
 # Pollutes: BASH_REMATCH, consider rewrite in extglob.
 depset_contains(){
-	[[ "$PKGDEP" =~ (^| )$1(_|[<>=]=[^ ]*)?( |$) ]]
+	local i;
+	for i in "$PKGDEP"; do
+		[[ "$i" =~ $1(_|[<>=]=[^ ]*)? ]] && break
+	done
+	false
 }
 depset_add(){
 	[[ "$1" && "$1" != "$PKGNAME" ]] || return 2
-	depset_contains $1 && return 0
-	PKGDEP+=" $1"
+	! depset_contains "$1" || return 0
+	PKGDEP+=("$1")
 	abinfo "Added dependency $1$([ "$2" ] && echo from $2)."
 }
