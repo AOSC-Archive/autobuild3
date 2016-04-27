2271947bf2cf27ad60a2ad81ab97d8a3e21de34a
Mingcong Bai <jeffbai@aosc.xyz>
2016-04-26 18:00:37 -0600

    qtproj: fix QT_SELECT usage

--- build/20-qtproj.sh
+++ build/20-qtproj.sh
@@ -8,10 +8,10 @@ build_qtproj_probe(){
 	[ "$QT_SELECT" ] || 
 	if bool $USEQT5; then
 		abwarn "\$USEQT5 is now deprecated. Use QT_SELECT=5."
-		QT_SELECT=qt5
+		QT_SELECT=5
 	elif bool $USEQT4; then
 		abwarn "\$USEQT4 is now deprecated. Use QT_SELECT=4."
-		QT_SELECT=qt4
+		QT_SELECT=4
 	else
 		abicu "Qt version unspecified => default."
 		QT_SELECT=default
