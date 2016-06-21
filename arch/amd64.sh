2824d742cd8f4d1ec39783991188a331e60ba268
Mingcong Bai <jeffbai@aosc.xyz>
2016-06-21 17:12:34 +0800

    arch/amd64: fix #94

--- arch/amd64.sh
+++ arch/amd64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ##arch/amd64.sh: Build definitions for amd64.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-march=x86-64 -mtune=core2 -msse -msse2 -msse3 '
+CFLAGS_COMMON_ARCH='-march=x86-64 -mtune=core2 -msse -msse2 -msse3 -ftree-vectorize '
