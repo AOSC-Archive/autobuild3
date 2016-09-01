4c758e288e87e45fda995e4bc9c0ba0fe01fb10b
Mingcong Bai <jeffbai@aosc.xyz>
2016-08-31 22:02:18 -0500

    common_switches: clang fallback

--- arch/_common_switches.sh
+++ arch/_common_switches.sh
@@ -5,3 +5,9 @@ if ((AB_FLAGS_SSP)); then CFLAGS_COMMON+='-fstack-protector-strong --param=ssp-b
 if ((AB_FLAGS_RRO)); then LDFLAGS_COMMON+='-Wl,-z,relro '; fi
 if ((AB_FLAGS_NOW)); then LDFLAGS_COMMON+='-Wl,-z,now '; fi
 if ((AB_FLAGS_FTF)); then CPPFLAGS_COMMON+='-D_FORTIFY_SOURCE=2 '; fi
+
+# Clang can handle PIE and PIC properly, let it do the old job.
+if bool $USECLANG; then
+	if ((AB_FLAGS_PIC)); then LDFLAGS_COMMON+='-fPIC ' CFLAGS_COMMON+='-fPIC '; fi
+	if ((AB_FLAGS_PIE)); then LDFLAGS_COMMON+='-fPIE -pie ' CFLAGS_COMMON+='-fPIE '; fi
+fi
