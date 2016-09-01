66316b51b250e0af4db0347923a30a9b55373b5a
Mingcong Bai <jeffbai@aosc.xyz>
2016-08-31 21:53:20 -0500

    build/15-perl.sh: adapt for Core 4 vendor dir

--- build/15-perl.sh
+++ build/15-perl.sh
@@ -9,7 +9,7 @@ build_perl_probe(){
 
 build_perl_build(){
 	BUILD_START
-	yes | perl Makefile.PL
+	yes | PERL_MM_USE_DEFAULT=1 perl Makefile.PL INSTALLDIRS=vendor
 	BUILD_READY
 	yes | make $MAKE_AFTER
 	BUILD_FINAL
