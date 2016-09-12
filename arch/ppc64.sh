1f8b1df4a73d586a2d527d6089abdcd726f0ee9f
Mingcong Bai <jeffbai@aosc.xyz>
2016-09-11 21:06:59 -0500

    arch/ppc64: further generalize flags

--- arch/ppc64.sh
+++ arch/ppc64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ##arch/ppc64.sh: Build definitions for PowerPC64 (PowerPC G5+)
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=' -m64 -mtune=G5 -maltivec -maltivec=be -mabi=altivec -mcmodel=medium -msecure-plt -mhard-float '
+CFLAGS_COMMON_ARCH=' -m64 -mtune=G5 -maltivec -maltivec=be -mabi=altivec -msecure-plt -mhard-float '
