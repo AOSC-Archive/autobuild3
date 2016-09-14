524cc035d60154f3c60c38abcf68d9d83d13c2e1
Mingcong Bai <jeffbai@aosc.xyz>
2016-09-14 13:34:31 -0500

    _common_switches: use replacement for O3

--- arch/_common_switches.sh
+++ arch/_common_switches.sh
@@ -6,7 +6,7 @@ if ((AB_FLAGS_RRO)); then LDFLAGS_COMMON+=' -Wl,-z,relro '; fi
 if ((AB_FLAGS_NOW)); then LDFLAGS_COMMON+=' -Wl,-z,now '; fi
 if ((AB_FLAGS_FTF)); then CPPFLAGS_COMMON+=' -D_FORTIFY_SOURCE=2 '; fi
 if ((AB_FLAGS_SPECS)); then LDFLAGS_COMMON+=" -specs=$AB/specs/hardened-ld "; CFLAGS_COMMON+=" -specs=$AB/specs/hardened-cc1 "; fi
-if ((AB_FLAGS_O3)); then CFLAGS_COMMON+=' -O3 '; fi
+if ((AB_FLAGS_O3)); then CFLAGS_COMMON_OPTI="${CFLAGS_COMMON_OPTI/O2/O3}"; fi
 
 # Clang can handle PIE and PIC properly, let it do the old job.
 if bool $USECLANG; then
