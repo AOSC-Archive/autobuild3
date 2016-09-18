4097ac636947269dcff1eb406b85a6c7e2f5d2b4
Mingcong Bai <jeffbai@aosc.xyz>
2016-09-18 08:20:09 -0500

    filter: generalize perl filter

--- filter/perl.sh
+++ filter/perl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##filter/perl_local: Removes perllocal.pod.
 ##@copyright GPL-2.0+
-filter_perllocal(){
+filter_perl(){
 	abinfo "Removed perllocal.pod."
 	find "$PKGDIR" -name perllocal.pod -delete
 	abinfo "Removed .packlist."
