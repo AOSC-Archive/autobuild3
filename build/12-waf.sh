6e974f6778cdf40f9af84d6eb56755917093880a
Mingcong Bai <jeffbai@aosc.xyz>
2016-09-19 14:52:03 -0500

    12-waf: fix functionality

--- build/12-waf.sh
+++ build/12-waf.sh
@@ -10,9 +10,9 @@ build_waf_probe(){
 build_waf_build(){
 	local _ret
 	BUILD_START
-	python2 waf ${WAF_DEF} ${WAF_AFTER}
+	python2 waf configure ${WAF_DEF} ${WAF_AFTER}
 	BUILD_READY
-	python2 waf build ${ABMK} ${MAKE_AFTER}
+	python2 waf build ${ABMK} ${MAKE_AFTER} ${MAKEFLAGS}
 	BUILD_FINAL
 	python2 waf install --destdir=${PKGDIR} || _ret=$PIPESTATUS
 	return $_ret
