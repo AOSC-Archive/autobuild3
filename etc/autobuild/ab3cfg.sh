49614c14f5fcf13ed893a99d16011822bf7a1a88
Mingcong Bai <jeffbai@aosc.xyz>
2016-09-29 15:52:51 -0500

    ab3cfg: disable RPM by default

--- etc/autobuild/ab3cfg.sh
+++ etc/autobuild/ab3cfg.sh
@@ -2,5 +2,5 @@
 ##Autobuild user config override
 # See a list of options in ab3_defcfg.sh.
 ABMPM=dpkg  # Your main PM
-ABAPMS=rpm  # Other PMs
+ABAPMS=     # Other PMs
 : ${MTER='AOSC Buildbot <aosc@members.fsf.org>'} # And your name and email
