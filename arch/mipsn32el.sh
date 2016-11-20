0153d6dbee6c01cd75dc126314deefecea85505b
Mingcong Bai <jeffbai@aosc.xyz>
2016-11-20 17:36:31 -0600

    arch/mipsn32el: remove config

--- arch/mipsn32el.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-##arch/mipsel.sh: Build definitions for mipsel (loongson 2f best).
-##@copyright GPL-2.0+
-# FIXME: Loongson workaround added as suggested by biergaizi, do this break anything?
-CFLAGS_COMMON_ARCH='-march=mips3 -mtune=loongson2f -mabi=n32 -Wa,-mfix-loongson2f-nop '
-LDFLAGS_COMMON_ARCH='-Wl,--hash-style=sysv '
