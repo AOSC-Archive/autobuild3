5de62d0f67cb7ddb1b0552d173b1ae6023e0a14e
Mingcong Bai <jeffbai@aosc.xyz>
2016-11-20 17:39:40 -0600

    arch/mips64el: build config

---
+++ arch/mips64el.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##arch/mips64el.sh: Build definitions for mipsel (loongson 2f best).
+##@copyright GPL-2.0+
+# Configuration confirmed in #41@aosc-os-core.
+# Suggested, discussed by:
+#   - Junde Yhi <lmy441900@aosc.xyz>
+#   - Artoria Wang <arthur2e5@aosc.xyz>
+#   - Bob Cao <bobcao3@aosc.xyz>
+#   - Mingcong Bai <jeffbai@aosc.xyz>
+
+# Note: Likely branch optimization is deprecated as of MIPS64.
+#       This optimization option is removed for MIPS64r6 target.
+CFLAGS_COMMON_ARCH=' -mabi=64 -march=mips64r2 -mtune=loongson3a -mno-branch-likely -fprefetch-loop-arrays '
