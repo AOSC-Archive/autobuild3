c7aa4d9914dc64721c2a7f879c1e8522ea0943d0
Icenowy Zheng <icenowy@aosc.xyz>
2017-02-11 14:03:52 +0800

    10-qa_sections: deal with caps with /
    Signed-off-by: Icenowy Zheng <icenowy@aosc.xyz>

--- proc/10-qa_sections.sh
+++ proc/10-qa_sections.sh
@@ -2,5 +2,10 @@
 ##11-qasec.sh: Kick the bucket if the section looks bad
 ##@copyright GPL-2.0+
 bool $ABQA || return
-grep -qF "$PKGSEC" "$AB/sets/section" ||
-	abicu "QA: $PKGSEC not in sets/section."
+if echo "$PKGSEC" | grep -q '/'; then
+	QASEC="$(echo $PKGSEC | cut -d / -f 2-)"
+else
+	QASEC="$PKGSEC"
+fi
+grep -qF "$QASEC" "$AB/sets/section" ||
+	abicu "QA: $QASEC not in sets/section."
