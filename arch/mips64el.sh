4a38899945adf09552e5bdf337e52bfe8c99963f
Junde Yhi <lmy441900@aosc.io>
2017-04-15 00:20:53 +0800

    arch/mips64el: remove -mno-branch-likely
    This is unnecessary. According to GCC Manual:
    
      ... An exception is for the MIPS32 and MIPS64 architectures and
      processors that implement those architectures; for those, Branch
      Likely instructions are not be generated by default because the
      MIPS32 and MIPS64 architectures specifically deprecate their use.
    
      (https://gcc.gnu.org/onlinedocs/gcc-6.3.0/gcc/MIPS-Options.html#MIPS-Options)
    
    `gcc -Q --help=target` Also gives the default option:
    
      ...
      -mbranch-likely             		[disabled]
    
    Thus, `-mno-branch-likely` makes no difference. Removing it.

--- arch/mips64el.sh
+++ arch/mips64el.sh
@@ -8,6 +8,4 @@
 #   - Bob Cao <bobcao3@aosc.xyz>
 #   - Mingcong Bai <jeffbai@aosc.xyz>
 
-# Note: Likely branch optimization is deprecated as of MIPS64.
-#       This optimization option is removed for MIPS64r6 target.
-CFLAGS_COMMON_ARCH=' -mabi=64 -march=mips64r2 -mtune=loongson3a -mno-branch-likely '
+CFLAGS_COMMON_ARCH=' -mabi=64 -march=mips64r2 -mtune=loongson3a '
