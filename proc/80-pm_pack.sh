e4e156991a8dc4c460376d90b25cb26cbac08ca4
Icenowy Zheng <icenowy@aosc.io>
2017-06-04 18:23:06 +0800

    drop automatically dummy conffiles file generation
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- proc/80-pm_pack.sh
+++ proc/80-pm_pack.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ##proc/pack: packs the package.
 ##@copyright GPL-2.0+
-touch "$SRCDIR/autobuild/conffiles"
 for i in $ABMPM $ABAPMS; do
 	. "$AB"/pm/"$i"/pack || aberr $i packing returned $?.
 done
