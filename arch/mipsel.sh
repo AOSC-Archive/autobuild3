9f5738059c27037ce68e3cd801936d301d3b9600
Junde Yhi <lmy441900@aosc.xyz>
2017-06-13 17:09:38 +0800

    arch/mipsel: explicitly specify -fPIC on mipsel

--- arch/mipsel.sh
+++ arch/mipsel.sh
@@ -2,5 +2,6 @@
 ##arch/mipsel.sh: Build definitions for mipsel (optimized for Loongson 2F).
 ##@copyright GPL-2.0+
 # FIXME: Loongson workaround added as suggested by biergaizi, do this break anything?
-CFLAGS_COMMON_ARCH='-march=mips2 -mtune=loongson2f -mabi=32 -Wa,-mfix-loongson2f-nop '
+# XXX: Global -fPIC flag, since GCC will refuse to link objects without explicitly specifying -fPIC on mipsel.
+CFLAGS_COMMON_ARCH='-march=mips2 -mtune=loongson2f -mabi=32 -Wa,-mfix-loongson2f-nop -fPIC '
 #LDFLAGS_COMMON_ARCH='-Wl,--hash-style=sysv '
