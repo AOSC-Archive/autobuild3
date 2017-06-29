d146b34acfd90ad1b02579c47c6c4e306523c245
Icenowy Zheng <icenowy@aosc.io>
2017-06-29 13:48:36 +0800

    50-build_exec: die rather than icu when PKGDIR is not created properly
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- proc/50-build_exec.sh
+++ proc/50-build_exec.sh
@@ -26,7 +26,7 @@ done
 
 build_${ABTYPE}_build || abdie "Build failed: $?."
 
-[ -d "$PKGDIR" ] || abicu "50-build: Suspecting build failure due to missing PKGDIR."
+[ -d "$PKGDIR" ] || abdie "50-build: Suspecting build failure due to missing PKGDIR."
 #[ -d "`arch_findfile overrides`" ] && cp -rla "`arch_findfile overrides`"/* "$PKGDIR/"
 [ -d "`arch_findfile overrides`" ] && cp -ra "`arch_findfile overrides`"/* "$PKGDIR/"
 arch_loadfile_strict beyond
