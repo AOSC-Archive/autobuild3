1d3092a2c778e9fa1ae8725d575c329e49782095
Icenowy Zheng <icenowy@aosc.io>
2017-06-29 13:01:04 +0800

    13-build_verinfo: this script shouldn't fail
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- proc/13-build_versinfo.sh
+++ proc/13-build_versinfo.sh
@@ -2,7 +2,8 @@
 ##proc/versionmeta: AOSC bugurl in version output
 ##@copyright GPL-2.0+
 
-abtrylib pm || return
-. /etc/lsb-release || return
+abtrylib pm || return 0
+. /etc/lsb-release || return 0
 : ${BUGURL:=https://bugs.anthonos.org}
 AUTOTOOLS_DEF+=" --with-pkgversion=$(pm_dumpver)_${DISTRIB_ID// /_} --with-bugurl=$BUGURL "
+true
