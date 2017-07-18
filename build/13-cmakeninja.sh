ea3cbb50425bc554cfac1d665a4ef0f3fb7ee88a
liushuyu <liushuyu011@gmail.com>
2017-07-18 22:02:47 +0800

    cmake + ninja: new build type

---
+++ build/13-cmakeninja.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# 12-cmakeninja.sh: Builds cmake with Ninja
+##@copyright GPL-2.0+
+(abtryexe cmake && abtryexe ninja) || ablibret
+
+build_cmakeninja_probe(){
+	[ -f CMakeLists.txt ]
+}
+
+build_cmakeninja_build(){
+	local _ret
+	ABSHADOW=${ABSHADOW_CMAKE-$ABSHADOW}
+	if bool $ABSHADOW
+	then
+		rm -rf build
+		mkdir build || abdie "Failed creating \$SRCDIR/build"
+		cd build
+	fi
+	BUILD_START
+	cmake $SRCDIR $CMAKE_DEF $CMAKE_AFTER -GNinja
+	BUILD_READY
+	ninja $ABMK $MAKE_AFTER
+	BUILD_FINAL
+	DESTDIR=$PKGDIR ninja install || _ret=$PIPESTATUS
+	if bool $ABSHADOW
+	then
+		cd ..
+	fi
+	return $_ret
+}
+ABBUILDS+=' cmakeninja'
