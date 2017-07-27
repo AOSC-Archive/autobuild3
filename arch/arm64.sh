f10ad18c8ff8f4d811b6b4c3886d749abb999060
Icenowy Zheng <icenowy@aosc.io>
2017-07-28 04:43:30 +0800

    arch: manually specify -fno-omit-frame-pointer on ARM64
    The unwind code in libgcc seems to still use the frame pointer for
    unwinding even when dwarf2 is available.
    
    Don't omit frame pointer by default on ARM64.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- arch/arm64.sh
+++ arch/arm64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ##arch/arm64.sh: Build definitions for arm64.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-march=armv8-a -mtune=cortex-a53 '
+CFLAGS_COMMON_ARCH='-march=armv8-a -mtune=cortex-a53 -fno-omit-frame-pointer'
 LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/arm64/usr/lib -L/var/ab/cross-root/arm64/usr/lib '
