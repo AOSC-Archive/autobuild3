68ffb863940134df2d3cd13135dd629e2df47424
Mingcong Bai <jeffbai@aosc.xyz>
2017-09-22 09:00:31 -0500

    qa: add check for loose files in certain directories

---
+++ proc/71-qa_lingering_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##71-qa_lingering_files: Check for lingering files.
+##@copyright GPL-2.0+
+bool $ABQA || return
+for i in "$PKGDIR"/* "$PKGDIR"/usr/* "$PKGDIR"/usr/share/* \
+	if test -f "$i"; then
+		abicu "QA: Possible lingering file found at $i, incorrect install location?"
+	fi
+done
