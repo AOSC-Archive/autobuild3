311d08a05edd23b9d0ff094d996a6c5e208d8d60
Mingcong Bai <jeffbai@aosc.xyz>
2017-09-22 14:47:45 -0500

    proc/71-qa_lingering_files: fix syntax error

--- proc/71-qa_lingering_files.sh
+++ proc/71-qa_lingering_files.sh
@@ -2,7 +2,7 @@
 ##71-qa_lingering_files: Check for lingering files.
 ##@copyright GPL-2.0+
 bool $ABQA || return
-for i in "$PKGDIR"/* "$PKGDIR"/usr/* "$PKGDIR"/usr/share/* \
+for i in "$PKGDIR"/* "$PKGDIR"/usr/* "$PKGDIR"/usr/share/*; do
 	if test -f "$i"; then
 		abicu "QA: Possible lingering file found at $i, incorrect install location?"
 	fi
