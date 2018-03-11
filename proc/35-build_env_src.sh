afd22e258a7c13ac356704de35bbfa364c39a595
Mingcong Bai <jeffbai@aosc.xyz>
2018-03-11 15:25:15 -0500

    proc: add proc 35-build_env_src

---
+++ proc/35-build_env_src.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+##proc/build_env_src: Source build environment definition scripts.
+##@copyright GPL-2.0+
+for i in /etc/profile.d/*.sh;
+    abinfo "Sourcing environment definition script $i ..."
+    source "$i" || abdie "Error sourcing environment definition."
+fi
