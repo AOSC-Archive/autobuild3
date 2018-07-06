5b5853c9a6f94ab1a1f9a8e039a8e9c27cd90369
Mingcong Bai <jeffbai@aosc.xyz>
2018-07-06 10:13:04 -0500

    proc/35-build_env_src: fix syntax

--- proc/35-build_env_src.sh
+++ proc/35-build_env_src.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##proc/build_env_src: Source build environment definition scripts.
 ##@copyright GPL-2.0+
-for i in /etc/profile.d/*.sh;
+for i in /etc/profile.d/*.sh; done
     abinfo "Sourcing environment definition script $i ..."
     source "$i" || abdie "Error sourcing environment definition."
-fi
+done
