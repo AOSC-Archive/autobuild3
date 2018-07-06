07cee5ad0c6de33073c64b40e7437716da38f575
Mingcong Bai <jeffbai@aosc.xyz>
2018-07-06 10:19:27 -0500

    proc/35-build_env_src: fix typo

--- proc/35-build_env_src.sh
+++ proc/35-build_env_src.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##proc/build_env_src: Source build environment definition scripts.
 ##@copyright GPL-2.0+
-for i in /etc/profile.d/*.sh; done
+for i in /etc/profile.d/*.sh; do
     abinfo "Sourcing environment definition script $i ..."
     source "$i" || abdie "Error sourcing environment definition."
 done
