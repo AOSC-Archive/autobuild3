0d6282c1daca4af66a7fe1d15f5c7f78c7ddaed6
Mingcong Bai <jeffbai@aosc.xyz>
2018-09-02 01:02:18 -0500

    build/20-ruby: fix build with current acbs

--- build/20-ruby.sh
+++ build/20-ruby.sh
@@ -3,18 +3,10 @@
 ##@copyright GPL-2.0+
 abtryexe ruby gem || ((!ABSTRICT)) || ablibret
 
-build_ruby_probe(){
-	local _gems _gem
-	_gems=(*.gem)
-	((${#_gems[@]})) || return 1
-	for _gem in "${_gems[@]}"; do
-		[ -f "$_gem" ] && return 0
-	done
-	return 1
-}
-
 build_ruby_build(){
   GEMDIR="$(ruby -e'puts Gem.default_dir')"
+  abwarn "For recent ACBS change which now saves all files with a .bin suffix."
+  mv -fv $PKGNAME-$PKGVER.{bin,gem} || true
   gem install --ignore-dependencies --no-user-install \
     -i "$PKGDIR/$GEMDIR" -n "$PKGDIR/usr/bin" $PKGNAME-$PKGVER.gem
   rm "$PKGDIR/$GEMDIR/cache/$PKGNAME-$PKGVER.gem"
