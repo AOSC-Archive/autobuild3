a5961a5c682b7631dd510d9ff5d47a8bfe7a8bc9
Mingcong Bai <jeffbai@aosc.xyz>
2018-10-25 14:29:52 -0500

    arch/amd64_avx2+: more details in comment, remove redundant -march=

--- arch/amd64_avx2+.sh
+++ arch/amd64_avx2+.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ##arch/amd64_avx2.sh: Build definitions for amd64 with AVX2 support.
+##                    Intel Haswell and AMD Zen or later processors.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=' -march=x86-64 -march=haswell '
+CFLAGS_COMMON_ARCH=' -march=haswell '
