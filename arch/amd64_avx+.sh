a5e5c53ab0f62fd2e8a17a2dbf56df3fc3f56c76
Mingcong Bai <jeffbai@aosc.xyz>
2018-10-25 14:30:23 -0500

    arch/amd64_avx+: new, for SNB/Bulldozer or newer

---
+++ arch/amd64_avx+.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+##arch/amd64_avx.sh: Build definitions for amd64 with AVX support.
+##                   Intel SandyBridge and AMD Bulldozer or later processors.
+##@copyright GPL-2.0+
+CFLAGS_COMMON_ARCH=' -march=sandybridge '
