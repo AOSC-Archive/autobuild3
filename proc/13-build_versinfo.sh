132fcdc55b907ab8b365e2ba28e67a11afad7c35
Junde Yhi <lmy441900@aosc.xyz>
2019-01-24 16:15:58 +0800

    proc/13-build_versinfo: fix deprecated bugurl
    bugs.anthonos.org has long beed deprecated and should be replaced
    by the new one. Packages that have different bug reporting
    locations should override this in $AUTOTOOLS_AFTER.
    
    Also fix a bug where /etc/lsb-release is read, in which $BUGURL is
    used to substitute the default value, but /etc/lsb-release does not
    have such entry.

--- proc/13-build_versinfo.sh
+++ proc/13-build_versinfo.sh
@@ -3,7 +3,7 @@
 ##@copyright GPL-2.0+
 
 abtrylib pm || return 0
-. /etc/lsb-release || return 0
-: ${BUGURL:=https://bugs.anthonos.org}
-AUTOTOOLS_DEF+=" --with-pkgversion=$(pm_dumpver)_${DISTRIB_ID// /_} --with-bugurl=$BUGURL "
+. /etc/os-release || return 0
+: ${BUG_REPORT_URL:=https://github.com/AOSC-Dev/aosc-os-abbs/issues/}
+AUTOTOOLS_DEF+=" --with-pkgversion=$(pm_dumpver)_${DISTRIB_ID// /_} --with-bugurl=$BUG_REPORT_URL "
 true
