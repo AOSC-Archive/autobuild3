862c9b03b52831b3c649855234078bb1922f4a22
Junde Yhi <lmy441900@aosc.xyz>
2019-01-24 16:02:01 +0800

    arch/mips64{,r2}el: add revised support
    - arch/_common: add triplet for the two archs, remove RPM support
      (because it is deprecated and I have no idea how to fill this)

---
+++ arch/mips64el.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+##arch/mipsel.sh: Build definitions for mipsel (optimized for Loongson 2F).
+##@copyright GPL-2.0+
+
+# '-mabi=64 -march=mips3 -mtune=loongson2f' are configured to be the default for GCC
+
+# Loongson 2F workaround added (as suggested by biergaizi) to fix bugs in early batches (1 and 2)
+CFLAGS_COMMON_ARCH='-Wa,-mfix-loongson2f-nop,-mfix-loongson2f-jump '
--- arch/mips64r2el.sh
+++ arch/mips64r2el.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ##arch/mips64el.sh: Build definitions for mip64r2el (optimized for Loongson 3A).
 ##@copyright GPL-2.0+
+
 # Configuration of old mips64el originally confirmed in #41@aosc-os-core.
 # Suggested, discussed by:
 #   - Junde Yhi <lmy441900@aosc.xyz>
@@ -9,12 +10,13 @@
 #   - Mingcong Bai <jeffbai@aosc.xyz>
 # Switched to mips64r2el by:
 #   - Jiaxun Yang <jiaxun.yang@flygoat.com>
+# Refactor modification by:
+#   - Junde Yhi <lmy441900@aosc.xyz>
 
+# '-mabi=64 -march=mips64r2 -mtune=loongson3a' are configured to be the default for GCC
+# '-mno-madd4' is configured to be the default for GCC
 
-CFLAGS_COMMON_ARCH='-EL  -mabi=64 -march=mips64r2 -mtune=loongson3a  -mllsc -mno-madd4'
+# Loongson 3 workaround added to fix faulty design that may cause deadlocks
+CFLAGS_COMMON_ARCH='-Wa,-mfix-loongson3-llsc,-mfix-loongson3-load '
+
 LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/mips64r2el/usr/lib -L/var/ab/cross-root/mips64r2el/usr/lib '
-
-# Loongson binutils fixes -mfix-loongson3-llsc, -mfix-loongson3-load is disableed. And should
-# be enabled in binutils patch.
-# Disable madd4 instructions beacuse Loongson have madd4 implementation that is different
-# from standard mips64r2. 
