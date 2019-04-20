caafab10a50f4fad465babee58bdf40e049550a1
Mingcong Bai <jeffbai@aosc.xyz>
2019-04-20 04:11:21 +0000

    arch: i586 registers

--- arch/_common.sh
+++ arch/_common.sh
@@ -14,6 +14,7 @@ ARCH_TARGET['arm64']=aarch64-aosc-linux-gnu
 ARCH_TARGET['powerpc']=powerpc-aosc-linux-gnu
 ARCH_TARGET['ppc64']=powerpc64-aosc-linux-gnu
 ARCH_TARGET['riscv64']=riscv64-aosc-linux-gnu
+ARCH_TARGET['i586']=i586-aosc-linux-gnu
 ARCH_RPM['amd64']=x86_64
 ARCH_RPM['amd64/avx+']=x86_64
 ARCH_RPM['amd64/avx2+']=x86_64
@@ -24,6 +25,7 @@ ARCH_RPM['powerpc']=ppc
 ARCH_RPM['ppc64']=ppc64
 ARCH_RPM['noarch']=noarch
 ARCH_RPM['riscv64']=riscv64
+ARCH_RPM['i586']=i586
 # C Compiler Flags.
 CFLAGS_COMMON='-pipe -Wno-error '
 CFLAGS_COMMON_OPTI='-O2 '
