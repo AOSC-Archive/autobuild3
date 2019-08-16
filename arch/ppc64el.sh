64ee3b53d1d9ce39704a2dc9ad43aebb00f8166e
Mingcong Bai <jeffbai@aosc.io>
2019-08-15 20:59:50 -0500

    arch/ppc64el: specify more optimisation flags

--- arch/ppc64el.sh
+++ arch/ppc64el.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ##arch/ppc64el.sh: Build definitions for ppc64el.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-mcpu=power8 -mtune=power9'
+CFLAGS_COMMON_ARCH='-mcpu=power8 -mtune=power9 -mpower8-vector -mpower8-fusion -maltivec -mabi=altivec -msecure-plt -mvsx'
