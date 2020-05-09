5d3bbb8d7f7d25bd79131754ca5d9a22c60bde65
Mingcong Bai <jeffbai@aosc.xyz>
2020-05-08 23:36:16 -0500

    arch/armhf: fix header and cross flags

--- arch/armhf.sh
+++ arch/armhf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##arch/armel.sh: Build definitions for armel.
+##arch/armhf.sh: Build definitions for armhf.
 ##@copyright GPL-2.0+
 CFLAGS_COMMON_ARCH='-march=armv7-a -mtune=cortex-a7 -mfloat-abi=hard -mfpu=neon -mthumb '
-LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/armel/usr/lib -L/var/ab/cross-root/armel/usr/lib '
+LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/armhf/usr/lib -L/var/ab/cross-root/armhf/usr/lib '
