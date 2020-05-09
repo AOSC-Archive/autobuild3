41be235f518319e7ba2ed85dac353ff476ea6c10
Mingcong Bai <jeffbai@aosc.xyz>
2020-05-09 00:13:35 -0500

    proc/python_defines: add AB-prefix; export

--- proc/10-python_defines.sh
+++ proc/10-python_defines.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##python_defines: Dynamic Python version declaration for installation scripts
 ##@copyright GPL-2.0+
-PY2VER="$(python2 -c 'import sys; print("%s.%s" %sys.version_info[0:2])')"
-PY2SHORTVER="$(python2 -c 'import sys; print("%s%s" %sys.version_info[0:2])')"
-PY3VER="$(python3 -c 'import sys; print("%s.%s" %sys.version_info[0:2])')"
-PY3SHORTVER="$(python3 -c 'import sys; print("%s%s" %sys.version_info[0:2])')"
+export ABPY2VER="$(python2 -c 'import sys; print("%s.%s" %sys.version_info[0:2])')"
+export ABPY2SHORTVER="$(python2 -c 'import sys; print("%s%s" %sys.version_info[0:2])')"
+export ABPY3VER="$(python3 -c 'import sys; print("%s.%s" %sys.version_info[0:2])')"
+export ABPY3SHORTVER="$(python3 -c 'import sys; print("%s%s" %sys.version_info[0:2])')"
