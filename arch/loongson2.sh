80ba34291b22fce2efab086c74857689700f83ef
Mingcong Bai <jeffbai@aosc.xyz>
2020-05-09 00:01:41 -0500

    arch/loongson2: update flags

--- arch/loongson2.sh
+++ arch/loongson2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-##arch/mips64el.sh: Build definitions for mip64el (optimized for Loongson 2F).
+##arch/loongson2.sh: Build definitions for Loongson 2E/F.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-EL -mabi=64 -march=mips3 -mtune=loongson2e '
-LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/mips64el/usr/lib -L/var/ab/cross-root/mips64el/usr/lib '
+CFLAGS_COMMON_ARCH='-EL -mabi=64 -march=loongson2e -mtune=loongson2f '
+LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/loongson2/usr/lib -L/var/ab/cross-root/loongson2/usr/lib '
