71765b2b7ab809d750700ff3e8d9420555552570
Mingcong Bai <jeffbai@aosc.xyz>
2020-05-09 23:20:48 -0500

    arch/loongson2: -march=mip3 instead
    See https://wiki.aosc.io/en/minutes-20200510#on-loongson.

--- arch/loongson2.sh
+++ arch/loongson2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ##arch/loongson2.sh: Build definitions for Loongson 2E/F.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-mabi=64 -march=loongson2e -mtune=loongson2f -Wa,-mfix-loongson2f-jump,-mfix-loongson2f-nop '
+CFLAGS_COMMON_ARCH='-mabi=64 -march=mips3 -mtune=loongson2f -Wa,-mfix-loongson2f-jump,-mfix-loongson2f-nop '
 LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/loongson2/usr/lib -L/var/ab/cross-root/loongson2/usr/lib '
