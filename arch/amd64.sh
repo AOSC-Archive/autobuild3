42efd8b93d8650dd60bf75568e69fb864f1faca3
liushuyu <liushuyu011@gmail.com>
2020-05-10 20:58:27 -0600

    arch: fix archtecture flags

--- arch/amd64.sh
+++ arch/amd64.sh
@@ -2,4 +2,4 @@
 ##arch/amd64.sh: Build definitions for amd64.
 ##@copyright GPL-2.0+
 CFLAGS_COMMON_ARCH='-fomit-frame-pointer -march=x86-64 -mtune=sandybridge -msse3 '
-RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=sandybridge '
+RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=x86-64 '
--- arch/arm64.sh
+++ arch/arm64.sh
@@ -2,4 +2,5 @@
 ##arch/arm64.sh: Build definitions for arm64.
 ##@copyright GPL-2.0+
 CFLAGS_COMMON_ARCH='-march=armv8-a -mtune=cortex-a53'
+RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=generic '
 LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/arm64/usr/lib -L/var/ab/cross-root/arm64/usr/lib '
