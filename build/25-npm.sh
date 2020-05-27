b4500a7b1ef4c569d7e35a3c65d94f506c7c0695
liushuyu <liushuyu011@gmail.com>
2020-05-26 20:11:00 -0600

    build/25-npm.sh: revert back to use archive to install

--- build/25-npm.sh
+++ build/25-npm.sh
@@ -3,15 +3,14 @@
 ##@copyright GPL-2.0+
 abtryexe node npm || ((!ABSTRICT)) || ablibret
 
-#FIXME: NPM archives requires using a non-extracted tarball.
 build_npm_probe(){
-	[ -e $PKGNAME-$PKGVER.tgz ]
+	[ -f package.json ]
 }
 
 build_npm_build(){
-	# requires npm 5+
-	npm install -g --user root \
-                    --prefix "$PKGDIR"/usr . || abdie "Could not install from archives"
+	ln -sv "$SRCDIR/../$PKGNAME-$PKGVER."* "$PKGNAME-$PKGVER.tgz"
+	npm install --production -g --user root \
+                    --prefix "$PKGDIR"/usr "$PKGNAME-$PKGVER.tgz" || abdie "Could not install from archives"
 }
 
 ABBUILDS+=' npm'
