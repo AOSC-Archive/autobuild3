8f8c4010f4231af35704fc2079f92c74c89adc1c
liushuyu <liushuyu011@gmail.com>
2020-05-28 17:32:42 -0600

    qa: sections: simplify string matching

--- qa/pre/sections.sh
+++ qa/pre/sections.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 ##sections.sh: Kick the bucket if the section looks bad
 ##@copyright GPL-2.0+
-if echo "$PKGSEC" | grep -q '/'; then
-	QASEC="$(echo $PKGSEC | cut -d / -f 2-)"
-else
-	QASEC="$PKGSEC"
-fi
-
-grep -qF "$QASEC" "$AB/sets/section" ||
-	abdie "QA (E104): $QASEC not in sets/section."
+grep -qF "$PKGSEC" "$AB/sets/section" ||
+	abdie "QA (E104): $PKGSEC not in sets/section."
