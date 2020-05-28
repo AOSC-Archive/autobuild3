8067742209f969032cb5ee7faf938d0a07a843dd
Mingcong Bai <jeffbai@aosc.xyz>
2020-05-28 16:27:48 -0500

    meta: move proc/*qa* to qa/

--- proc/71-qa_lingering_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-##71-qa_lingering_files: Check for lingering files.
-##@copyright GPL-2.0+
-bool "$ABQA" || return
-for i in "$PKGDIR"/* "$PKGDIR"/usr/* "$PKGDIR"/usr/share/*; do
-	if test -f "$i"; then
-		abicu "QA: Possible lingering file found at $i, incorrect install location?"
-	fi
-done
