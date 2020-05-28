56d8a9c2cf84a738a8be6b0f32767ea034cdb4fe
Mingcong Bai <jeffbai@aosc.xyz>
2020-05-28 18:33:09 -0500

    qa: write errors/warning to log

--- qa/post/lingering_files.sh
+++ qa/post/lingering_files.sh
@@ -2,4 +2,6 @@
 ##lingering_files: Check for lingering files.
 ##@copyright GPL-2.0+
 FILES="$(find "$PKGDIR" "$PKGDIR/usr/" "$PKGDIR/usr/share/" -maxdepth 1 -type f -print)"
-[ ! -z "$FILES" ] && abicu "QA (E321): Lingering file(s) found, incorrect install location?\n $FILES" || true
+[ ! -z "$FILES" ] && \
+	(abicu "QA (E321): Lingering file(s) found, incorrect install location?\n $FILES" || true) | \
+		tee -a "$SRCDIR"/abqaerr.log
--- qa/post/permissions.sh
+++ qa/post/permissions.sh
@@ -2,7 +2,9 @@
 ##permissions: Check for incorrect permissions.
 ##@copyright GPL-2.0+
 FILES="$(find "$PKGDIR/usr/bin" -type f -not -executable -print)"
-[ ! -z "$FILES" ] && abicu "QA (E324): non-executable file(s) found in /usr/bin:\n$FILES"
+[ ! -z "$FILES" ] && \
+	abicu "QA (E324): non-executable file(s) found in /usr/bin:\n$FILES" | tee -a "$SRCDIR"/abqaerr.log
 
 FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.so*' -not -executable -print)"
-[ ! -z "$FILES" ] && abicu "QA (E324): non-executable shared object(s) found in /usr/lib:\n $FILES"
+[ ! -z "$FILES" ] && \
+	abicu "QA (E324): non-executable shared object(s) found in /usr/lib:\n $FILES" | tee -a "$SRCDIR"/abqaerr.log
