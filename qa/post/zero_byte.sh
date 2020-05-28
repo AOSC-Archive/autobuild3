fdbbcc8594268cebea5869e46fbbbd2723bbca82
liushuyu <liushuyu011@gmail.com>
2020-05-28 16:40:34 -0600

    qa: zero_byte: speed up the process

--- qa/post/zero_byte.sh
+++ qa/post/zero_byte.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 ##zero_byte: Check for zero-byte files.
 ##@copyright GPL-2.0+
-for i in `find "$PKGDIR" -type f`; do
-	if [ ! -s "$i" ]; then
-		abwarn "QA (W322): Zero-byte file found at $i."
-	fi
-done
+FILES="$(find "$PKGDIR" -type f -size 0 -print)"
+[ ! -z "$FILES" ] && abwarn "QA (W322): Zero-byte files found:\n $FILES."
