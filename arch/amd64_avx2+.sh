f32c41e6305d05a42cde898e3718f21acc887f20
Mingye Wang <arthur200126@gmail.com>
2020-06-27 00:05:01 +0800

    amd64_avx2: remove rdrnd
    Removing RDRND lets us support all the AVX2 CPUs in the setting. See https://sourceware.org/bugzilla/show_bug.cgi?id=23249#c12.

--- arch/amd64_avx2+.sh
+++ arch/amd64_avx2+.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ##arch/amd64_avx2.sh: Build definitions for amd64 with AVX2 support.
-##                    Intel Haswell and AMD Zen or later processors.
+##                    Intel Haswell+, AMD bdver4+, VIA eden-x4+.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH=' -march=haswell '
+CFLAGS_COMMON_ARCH=' -march=haswell -mno-rdrnd '
