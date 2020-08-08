301744334cc1f962f684f71619d142563e2345a0
Mingcong Bai <jeffbai@aosc.xyz>
2020-08-08 17:24:05 -0600

    arch/loongson2f: renamed from loongson2f_mmi
    - Let's not kid ourselves, what good does a n64 MIPS-III do?
    - MMI is now standard, make this port 2F-specific.
    - Drop -mfix-loongson2-jump, which breaks n64 binaries.

--- arch/loongson2.sh
+++ arch/loongson2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-##arch/loongson2.sh: Build definitions for Loongson 2E/F.
+##arch/loongson2.sh: Build definitions for Loongson 2F.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-mabi=64 -march=mips3 -mtune=loongson2f -Wa,-mfix-loongson2f-jump,-mfix-loongson2f-nop '
-LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/loongson2/usr/lib -L/var/ab/cross-root/loongson2/usr/lib '
+CFLAGS_COMMON_ARCH='-mabi=64 -march=loongson2f -mloongson-mmi -Wa,-mfix-loongson2f-nop '
--- arch/loongson2_mmi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-##arch/loongson2_mmi.sh: Optimisation overlay for MMI-enabled Loongson2 (2F).
-##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-mabi=64 -march=loongson2f -mloongson-mmi -Wa,-mfix-loongson2f-jump,-mfix-loongson2f-nop '
