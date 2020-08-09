3f5ae9307521fd2beef674cc76a2c98f053ae80c
Mingcong Bai <jeffbai@aosc.xyz>
2020-08-08 20:28:16 -0600

    arch/loongson2f: drop march to mips3
    - When using -march=loongson2f, our build host (Loongson 3A won't
      be able to run code generated with this flag enabled.

--- arch/loongson2.sh
+++ arch/loongson2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ##arch/loongson2.sh: Build definitions for Loongson 2F.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-mabi=64 -march=loongson2f -mloongson-mmi -Wa,-mfix-loongson2f-nop '
+CFLAGS_COMMON_ARCH='-mabi=64 -march=mips3 -mtune=loongson2f -mloongson-mmi -Wa,-mfix-loongson2f-nop '
