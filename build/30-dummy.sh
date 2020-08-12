dfbf7f4eeedf479426279a258a1b503a0559bb70
Mingcong Bai <jeffbai@aosc.xyz>
2020-08-12 07:01:20 +0000

    build/30-dummy: add dummy probing

--- build/30-dummy.sh
+++ build/30-dummy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 ##30-dummy.sh: Builds dummy/meta/transitional packages
 ##@copyright GPL-2.0+
-abtryexe node npm || ((!ABSTRICT)) || ablibret
+
+build_dummy_probe(){
+	true
+}
 
 build_dummy_build(){
 	abinfo "Creating a dummy package ..."
