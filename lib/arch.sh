1f39003ed9b3ce5c8368f6242f41b4ad8e2a0b9d
liushuyu <liushuyu011@gmail.com>
2020-08-16 14:30:06 -0600

    arch: do a syntax check before running the script

--- lib/arch.sh
+++ lib/arch.sh
@@ -69,7 +69,7 @@ arch_loadfile_strict(){
 	local _abarchf _arch_trymore=${arch_trymore:-1};
 	_abarchf="$(arch_findfile "$1")" || return $?;
 	shift;
-	echo -e 'trap - ERR; trap "abdie" ERR\n' > "${_abarchf}.wrap.sh"
+	echo -e 'trap - ERR; trap "abdie" ERR; bash -n "$0"\n' > "${_abarchf}.wrap.sh"
 	cat "$_abarchf" >> "${_abarchf}.wrap.sh"
 	echo -e '\ntrap - ERR' >> "${_abarchf}.wrap.sh"
 	. "${_abarchf}.wrap.sh" "$@";
