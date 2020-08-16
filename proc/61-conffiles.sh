6899a437ff848e1d93cd9a56f93c72d5435eed9b
Mingcong Bai <jeffbai@aosc.xyz>
2020-08-17 05:02:27 +0800

    proc/61-conffile: enhance functionalities
    - Tri-state detection: conffiles not found, conffiles too short/long,
      or conffiles perfecct.
    - When "conffiles not found" - generate autobuild/conffiles
      automatically while outputting a warning, along with the content
      appended to autobuild/conffiles.
    - When "conffiles too short/long" - print current autobuild/conffiles,
      acknowledging that file, then output a warning stating that there
      are more/less files found in "$PKGDIR"/etc.
    - When "conffiles perfect" - congratulates packager and moves on.

--- proc/61-conffiles.sh
+++ proc/61-conffiles.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 ##proc/conffiles.sh: Generate conffiles if not already written
 ##@copyright GPL-2.0+
-if [[ ! -e "$SRCDIR"/autobuild/conffiles && -d "$PKGDIR"/etc ]]; then
+
+# Generate a cached conffiles.
+find "$PKGDIR"/etc -type f -printf '/etc/%P\n' \
+	| sort > "$SRCDIR"/conffiles.ab
+
+if [[ -d "$PKGDIR"/etc && ! -e "$SRCDIR"/autobuild/conffiles ]]; then
 	abwarn 'Detected /etc in $PKGDIR, but autobuild/conffiles is not found - attempting generation ...'
-	find "$PKGDIR"/etc -type f -printf '/etc/%P\n' \
-		2>&1 | tee  "$SRCDIR"/autobuild/conffiles
+	abinfo 'Appending the following to autobuild/conffiles ...'
+	cat "$SRCDIR"/conffiles.ab \
+		2>&1 | sort | tee "$SRCDIR"/autobuild/conffiles
+elif ! diff "$SRCDIR"/conffiles.ab <(sort "$SRCDIR"/autobuild/conffiles) \
+	>/dev/null 2>&1; then
+	abinfo 'Found autobuild/conffiles - good job! Content as follows ...'
+        cat "$SRCDIR"/autobuild/conffiles | sort
+	abwarn 'However, there seems to be more/less files found in $PKGDIR/etc ...'
+	abwarn 'Please compare the following list of files against the current autobuild/conffiles ...'
+	cat "$SRCDIR"/conffiles.ab
+else
+	abinfo 'Found autobuild/conffiles - all good!'
 fi
