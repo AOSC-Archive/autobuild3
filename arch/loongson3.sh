519cc5a6f74606e822b1752f12336d4d1c16f090
Mingcong Bai <jeffbai@aosc.xyz>
2020-08-27 10:21:21 -0600

    arch/loongson3a: rename to loongson3

--- arch/loongson3.sh
+++ arch/loongson3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-##arch/loongson3a.sh: Build definitions for Loongson 3A/B 1000-4000+.
+##arch/loongson3.sh: Build definitions for Loongson 3A/B 1000-4000+.
 ##@copyright GPL-2.0+
 CFLAGS_COMMON_ARCH='-mabi=64 -march=gs464 -mtune=gs464e -mfix-loongson3-llsc '
