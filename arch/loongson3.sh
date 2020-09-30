03eed0cd972f24468a608791a31cae650871595e
Mingcong Bai <jeffbai@aosc.xyz>
2020-09-29 22:23:25 -0700

    arch/loongson3: differentiate gcc and clang flags

--- arch/loongson3.sh
+++ arch/loongson3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ##arch/loongson3.sh: Build definitions for Loongson 3A/B 1000-4000+.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-mabi=64 -march=gs464 -mtune=gs464e -mfix-loongson3-llsc '
+CFLAGS_GCC_ARCH='-mabi=64 -march=gs464 -mtune=gs464e -mfix-loongson3-llsc '
+CFLAGS_CLANG_ARCH='-mabi=64 -march=mips64r2 -mtune=mips64r2 '
