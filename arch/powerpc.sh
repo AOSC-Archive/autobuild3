87a95fc2a52ccb4e17d7de4cd939c0c02fa143a0
Mingcong Bai <jeffbai@aosc.xyz>
2020-10-17 20:54:24 -0700

    arch/powerpc: use -mcpu=G3 since platform-specific support is assumed

--- arch/powerpc.sh
+++ arch/powerpc.sh
@@ -7,4 +7,4 @@ CFLAGS_COMMON_OPTI='-Os '
 # Retro: Also disabling -ftree-vectorization which could potentially enlarge code size.
 CFLAGS_GCC_OPTI='-fira-loop-pressure -fira-hoist-pressure '
 
-CFLAGS_COMMON_ARCH=' -m32 -mtune=G4 -mno-altivec -msecure-plt -mhard-float '
+CFLAGS_COMMON_ARCH=' -m32 -mcpu=G3 -mtune=G4 -mno-altivec -msecure-plt -mhard-float '
