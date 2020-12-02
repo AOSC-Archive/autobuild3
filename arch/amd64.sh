8d7906e7fa48752b5f134d2ea43a43de9c1033be
Mingcong Bai <jeffbai@aosc.xyz>
2020-12-02 13:18:04 -0800

    arch/amd64: re-adjust SSE level to SSE2
    - This is a long-running misdesign on our part - most older (pre-AM2)
      AMD x86_64 processors does not support SSE3.
    - Instead of a full-scale rebuild, we will gradually rebuild packages.
      Starting with Core 8, AMD64 packages will be built using SSE2.
    - Rebuilds could also be requested if a user should run into issues
      with non-SSE3-capable AMD64 processors.

--- arch/amd64.sh
+++ arch/amd64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ##arch/amd64.sh: Build definitions for amd64.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-fomit-frame-pointer -march=x86-64 -mtune=sandybridge -msse3 '
+CFLAGS_COMMON_ARCH='-fomit-frame-pointer -march=x86-64 -mtune=sandybridge -msse2 '
 RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=x86-64 '
