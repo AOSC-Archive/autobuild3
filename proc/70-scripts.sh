f25432bbcc91e376730f5b2f9302c34e50177774
Icenowy Zheng <icenowy@aosc.io>
2020-12-05 22:30:57 +0800

    pm: switch to dpkg native triggers
    As we only care about dpkg now, drop AB3 triggers (which is modelled
    after dpkg triggers) and use dpkg one.
    
    In this case, we no longer need to have all packages that will trigger a
    package to be in the package's BUILDDEP, because dpkg can do this based
    on its DB. In addition, DPKG can postpone trigger actions to the end of
    a dpkg/apt command, saves some (much?) time.
    
    AB3 triggers will only react when /var/ab/triggered/XXX is exist, and
    new packages packed with dpkg native trigger won't have them, so the
    transition shouldn't be problematic.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- proc/70-scripts.sh
+++ proc/70-scripts.sh
@@ -9,22 +9,4 @@ for i in postinst prerm postrm preinst; do
 	chmod 755 abscripts/$i
 done
 
-if [ -e autobuild/triggers ]; then
-	[ $AB_DBG ] && abinfo "Triggers found."
-	echo "#!/bin/bash" > abscripts/trigger
-	echo "#!/bin/bash" > abscripts/triggered
-	echo "export PKGNAME=$PKGNAME" >> abscripts/trigger
-	cat $AB/trigger/lib.sh >> abscripts/trigger
-	cat autobuild/triggers >> abscripts/trigger
-	cat autobuild/postinst >> abscripts/triggered
-	chmod 755 abscripts/trigger{,ed}
-	mkdir -p abdist/var/ab/trigger{s,ed}
-	cp abscripts/trigger abdist/var/ab/triggers/$PKGNAME
-	cp abscripts/triggered abdist/var/ab/triggered/$PKGNAME
-fi
-
 recsr $AB/scriptlet/*.sh
-
-for i in /var/ab/triggers/*; do
-	[ "$(basename "${i##.sh}")" == "$PKGNAME" ] || "$i" || abwarn "trigger $i: $?"
-done
