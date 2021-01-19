ef3ddd13a7133571a3d39c96936de1e9584e5a33
Mingcong Bai <jeffbai@aosc.xyz>
2021-01-19 14:34:49 -0800

    proc/10-env-setup: new; set HOME, SHELL

---
+++ proc/10-env-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##10-env-setup: Setting up common environmental variables
+##@copyright GPL-2.0+
+
+abinfo 'Setting $SHELL to /bin/bash ...'
+export SHELL="/bin/bash"
+
+abinfo 'Setting $HOME to /tmp ...'
+export HOME="/tmp"
