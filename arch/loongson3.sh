e3081c0e7c407b89c97eafa0dae76757087a3ad7
liushuyu <liushuyu011@gmail.com>
2021-01-30 02:00:53 -0700

    arch/loongson3: use lld for rustc linking

--- arch/loongson3.sh
+++ arch/loongson3.sh
@@ -3,6 +3,4 @@
 ##@copyright GPL-2.0+
 CFLAGS_GCC_ARCH='-mabi=64 -march=gs464 -mtune=gs464e -mfix-loongson3-llsc '
 CFLAGS_CLANG_ARCH='-mabi=64 -march=mips64r2 -mtune=mips64r2 '
-# Disable full LTO on loongson3 (Rust will use scoped/local LTO)
-# Reason why bfd isn't working: https://bugs.llvm.org/show_bug.cgi?id=47872
-RUSTFLAGS_COMMON_OPTI_LTO='-Clink-arg=-flto -Clinker=clang -Clink-arg=-fuse-ld=gold '
+RUSTFLAGS_COMMON_OPTI_LTO="${RUSTFLAGS_COMMON_OPTI_LTO} -Clink-arg=-Wl,-z,notext "
