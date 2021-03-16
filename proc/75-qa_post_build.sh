a0980c7bb2af14b2a9266395886dd989b7191164
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-16 16:27:13 -0700

    proc/*qa*: fix file loading; test for zero-byte logs

--- proc/75-qa_post_build.sh
+++ proc/75-qa_post_build.sh
@@ -3,10 +3,10 @@
 ##@copyright GPL-2.0+
 bool $ABQA && \
 for i in "$AB"/qa/post/*; do
-	arch_loadfile_strict "$i"
+	. "$i"
 done
 
-[ -e "$SRCDIR"/abqawarn.log ] && \
+[[ -e "$SRCDIR"/abqawarn.log && ! -z "$SRCDIR"/abqawarn.log ]] && \
         abwarn "QA warning(s) found, please refer to abqawarn.log ..."
-[ -e "$SRCDIR"/abqaerr.log ] && \
-	abdie "QA error(s) found, please refer to abqaerr.log ..."
+[[ -e "$SRCDIR"/abqaerr.log && ! -z "$SRCDIR"/abqaerr.log ]] && \
+        abdie "QA error(s) found, please refer to abqaerr.log ..."
