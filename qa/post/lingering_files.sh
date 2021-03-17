001fa655fc1108115b1e3f56460734b39bb50f09
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-16 17:15:25 -0700

    qa/post/lingering_files: use full if branch

--- qa/post/lingering_files.sh
+++ qa/post/lingering_files.sh
@@ -2,6 +2,7 @@
 ##lingering_files: Check for lingering files.
 ##@copyright GPL-2.0+
 FILES="$(find "$PKGDIR" "$PKGDIR/usr/" "$PKGDIR/usr/share/" -maxdepth 1 -type f -print)"
-[ ! -z "$FILES" ] && \
-	(aberr "QA (E321): Lingering file(s) found, incorrect install location?\n $FILES" || true) | \
+if [ ! -z "$FILES" ]; then
+	aberr "QA (E321): Lingering file(s) found (incorrect install location?):\n    $FILES" | \
 		tee -a "$SRCDIR"/abqaerr.log
+fi
