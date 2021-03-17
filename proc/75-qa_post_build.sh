1610821bd9e36acbe7b1e247b6ff757f41d6118f
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-16 17:14:16 -0700

    proc/*qa*: use -s to test for existing, non-zero-byte files

--- proc/75-qa_post_build.sh
+++ proc/75-qa_post_build.sh
@@ -6,7 +6,10 @@ for i in "$AB"/qa/post/*; do
 	. "$i"
 done
 
-[[ -e "$SRCDIR"/abqawarn.log && ! -z "$SRCDIR"/abqawarn.log ]] && \
+if [ -s "$SRCDIR"/abqawarn.log ]; then
         abwarn "QA warning(s) found, please refer to abqawarn.log ..."
-[[ -e "$SRCDIR"/abqaerr.log && ! -z "$SRCDIR"/abqaerr.log ]] && \
+fi
+
+if [ -s "$SRCDIR"/abqaerr.log ]; then
         abdie "QA error(s) found, please refer to abqaerr.log ..."
+fi
