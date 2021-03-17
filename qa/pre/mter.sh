7b3bffb2acab298c9891770eae865595109b0ca7
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-16 17:17:05 -0700

    qa/pre/mter: lint script

--- qa/pre/mter.sh
+++ qa/pre/mter.sh
@@ -2,5 +2,6 @@
 ##mter: Check maintainer information.
 ##@copyright GPL-2.0+
 if [[ "$MTER" = "Null Packager <null@aosc.xyz>" ]]; then
-	aberr "QA (E311): Maintainer information not set." | tee -a "$SRCDIR"/abqaerr.log
+	aberr "QA (E311): Maintainer information not set." | \
+		tee -a "$SRCDIR"/abqaerr.log
 fi
