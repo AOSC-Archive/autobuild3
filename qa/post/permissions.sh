79f7efe7bc33b0f960a6f2c9c3a8a718058d7b17
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-16 17:16:11 -0700

    qa/post/permissions: also test for executable .a/.o files

--- qa/post/permissions.sh
+++ qa/post/permissions.sh
@@ -2,9 +2,25 @@
 ##permissions: Check for incorrect permissions.
 ##@copyright GPL-2.0+
 FILES="$(find "$PKGDIR/usr/bin" -type f -not -executable -print)"
-[ ! -z "$FILES" ] && \
-	aberr "QA (E324): non-executable file(s) found in /usr/bin:\n$FILES" | tee -a "$SRCDIR"/abqaerr.log
+if [ ! -z "$FILES" ]; then
+	aberr "QA (E324): non-executable file(s) found in /usr/bin:\n    $FILES" | \
+		tee -a "$SRCDIR"/abqaerr.log
+fi
 
 FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.so*' -not -executable -print)"
-[ ! -z "$FILES" ] && \
-	aberr "QA (E324): non-executable shared object(s) found in /usr/lib:\n $FILES" | tee -a "$SRCDIR"/abqaerr.log
+if [ ! -z "$FILES" ]; then
+	aberr "QA (E324): non-executable shared object(s) found in /usr/lib:\n    $FILES" | \
+		tee -a "$SRCDIR"/abqaerr.log
+fi
+
+FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.a' -executable -print)"
+if [ ! -z "$FILES" ]; then
+	aberr "QA (E324): executable static object(s) found in /usr/lib:\n    $FILES" | \
+		tee -a "$SRCDIR"/abqaerr.log
+fi
+
+FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.o' -executable -print)"
+if [ ! -z "$FILES" ]; then
+	aberr "QA (E324): executable binary object(s) found in /usr/lib:\n    $FILES" | \
+		tee -a "$SRCDIR"/abqaerr.log
+fi
