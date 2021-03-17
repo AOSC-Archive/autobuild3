deffed2bbb54db9e2ccff1a1639889963b6f8674
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-16 17:16:37 -0700

    qa/post/zero_byte: use full if branch

--- qa/post/zero_byte.sh
+++ qa/post/zero_byte.sh
@@ -2,5 +2,7 @@
 ##zero_byte: Check for zero-byte files.
 ##@copyright GPL-2.0+
 FILES="$(find "$PKGDIR" -type f -size 0 -print)"
-[ ! -z "$FILES" ] && \
-	(abwarn "QA (W322): Zero-byte files found:\n $FILES." || true) | tee -a "$SRCDIR"/abqawarn.log
+if [ ! -z "$FILES" ]; then
+	abwarn "QA (W322): Zero-byte files found:\n $FILES." | \
+		tee -a "$SRCDIR"/abqawarn.log
+fi
