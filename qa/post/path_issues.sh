51e20422725fb7b1f207ebad22df9c4a8e56f1d3
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-16 18:34:30 -0700

    qa/post/path_issues.sh: check paths by depth (there has got to be a better way)

--- qa/post/path_issues.sh
+++ qa/post/path_issues.sh
@@ -42,12 +42,42 @@ ACCEPTABLE="
 "$PKGDIR"/usr/src
 "$PKGDIR"/var
 "
-PATHS="$(find "$PKGDIR" -maxdepth 2 -type d -print)"
+ACCEPTABLE2="
+"$PKGDIR"/usr/bin
+"$PKGDIR"/usr/include
+"$PKGDIR"/usr/lib
+"$PKGDIR"/usr/libexec
+"$PKGDIR"/usr/local
+"$PKGDIR"/usr/share
+"$PKGDIR"/usr/src
+"
+ACCEPTABLE3="
+"$PKGDIR"/usr/local/bin
+"$PKGDIR"/usr/local/include
+"$PKGDIR"/usr/local/lib
+"$PKGDIR"/usr/local/libexec
+"$PKGDIR"/usr/local/share
+"$PKGDIR"/usr/local/src
+"
+PATHS="$(find "$PKGDIR" -maxdepth 1 -type d -print)"
+PATHS2="$(find "$PKGDIR" -mindepth 2 -maxdepth 2 -type d -print)"
+PATHS3="$(find "$PKGDIR" -mindepth 3 -maxdepth 3 -type d -print)"
 for i in $PATHS; do
 	if ! echo $ACCEPTABLE | grep -q $i; then
 		aberr "QA (E321): found unexpected path(s) in package:\n    $i"
 	fi
 done
+for i in $PATHS2; do
+        if ! echo $ACCEPTABLE2 | grep -q $i; then
+                aberr "QA (E321): found unexpected path(s) in package:\n    $i"
+        fi
+done
+for i in $PATHS3; do
+        if ! echo $ACCEPTABLE3 | grep -q $i; then
+                aberr "QA (E321): found unexpected path(s) in package:\n    $i"
+        fi
+done
+
 
 for i in "$PKGDIR"/{dev,home,proc,sys,tmp}; do
 	if [ -e $i ]; then
