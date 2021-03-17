c0b6fadca1c34f53408338650cae1b5454ecf3cb
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-16 17:17:19 -0700

    qa/pre/sections: lint script

--- qa/pre/sections.sh
+++ qa/pre/sections.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ##sections.sh: Kick the bucket if the section looks bad
 ##@copyright GPL-2.0+
-(grep -qF "$PKGSEC" "$AB/sets/section" || aberr "QA (E104): $PKGSEC not in sets/section.") | \
-	tee -a "$SRCDIR"/abqaerr.log
+(grep -qF "$PKGSEC" "$AB/sets/section" || \
+	aberr "QA (E104): $PKGSEC not in sets/section.") | \
+		tee -a "$SRCDIR"/abqaerr.log
