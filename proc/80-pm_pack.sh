b8d14c446e4becffad12b6eafd961d1f04f79d65
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-19 11:02:37 -0700

    proc/pm_{pack,install,archive}: print an abinfo before operations

--- proc/80-pm_pack.sh
+++ proc/80-pm_pack.sh
@@ -2,5 +2,6 @@
 ##proc/pack: packs the package.
 ##@copyright GPL-2.0+
 for i in $ABMPM $ABAPMS; do
+	abinfo "Building $i package(s) ..."
 	. "$AB"/pm/"$i"/pack || aberr $i packing returned $?.
 done
--- proc/99-pm_archive.sh
+++ proc/99-pm_archive.sh
@@ -2,7 +2,8 @@
 ##proc/archive: archives the package.
 ##@copyright GPL-2.0+
 if [ "$ABARCHIVE" ]; then
-	abinfo "Using $ABARCHIVE as autobuild archiver."
+	abinfo "Archiving package(s) ..."
+	abinfo "Using $ABARCHIVE as autobuild archiver ..."
 	$ABARCHIVE "$PKGNAME" "$PKGVER" "$PKGREL" "$ABHOST"
 	# FIXME: should consult ABPACKAGE for the actual list of package to copy.
 	#   ~cth
@@ -10,5 +11,5 @@ if [ "$ABARCHIVE" ]; then
 		$ABARCHIVE "$PKGNAME"-dbg "$PKGVER" "$PKGREL" "$ABHOST"
 	fi
 else
-	abinfo "Not using an archiver."
+	abinfo "Not using an archiver, skipping ..."
 fi
