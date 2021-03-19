d8f9c5435747326d616703763a64bc125b5c31f4
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-19 10:25:31 -0700

    proc: split archive, move archive after install

--- proc/80-pm_pack.sh
+++ proc/80-pm_pack.sh
@@ -4,15 +4,3 @@
 for i in $ABMPM $ABAPMS; do
 	. "$AB"/pm/"$i"/pack || aberr $i packing returned $?.
 done
-
-if [ "$ABARCHIVE" ]; then
-	abinfo "Using $ABARCHIVE as autobuild archiver."
-	$ABARCHIVE "$PKGNAME" "$PKGVER" "$PKGREL" "$ABHOST"
-	# FIXME: should consult ABPACKAGE for the actual list of package to copy.
-	#   ~cth
-	if bool $ABSPLITDBG; then
-		$ABARCHIVE "$PKGNAME"-dbg "$PKGVER" "$PKGREL" "$ABHOST"
-	fi
-else
-	abinfo "Not using an archiver."
-fi
---
+++ proc/99-pm_archive.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+##proc/archive: archives the package.
+##@copyright GPL-2.0+
+if [ "$ABARCHIVE" ]; then
+	abinfo "Using $ABARCHIVE as autobuild archiver."
+	$ABARCHIVE "$PKGNAME" "$PKGVER" "$PKGREL" "$ABHOST"
+	# FIXME: should consult ABPACKAGE for the actual list of package to copy.
+	#   ~cth
+	if bool $ABSPLITDBG; then
+		$ABARCHIVE "$PKGNAME"-dbg "$PKGVER" "$PKGREL" "$ABHOST"
+	fi
+else
+	abinfo "Not using an archiver."
+fi
