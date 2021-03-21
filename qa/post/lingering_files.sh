5d350ac264e32feb10026ea2edad32c08453af6d
Mingcong Bai <jeffbai@aosc.xyz>
2021-03-21 13:18:26 -0700

    qa/post: silence more errors when collecting file lists

--- qa/post/lingering_files.sh
+++ qa/post/lingering_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##lingering_files: Check for lingering files.
 ##@copyright GPL-2.0+
-FILES="$(find "$PKGDIR" "$PKGDIR/usr/" "$PKGDIR/usr/share/" -maxdepth 1 -type f -print)"
+FILES="$(find "$PKGDIR" "$PKGDIR/usr/" "$PKGDIR/usr/share/" -maxdepth 1 -type f -print 2>/dev/null)"
 if [ ! -z "$FILES" ]; then
 	aberr "QA (E321): Lingering file(s) found (incorrect install location?):\n\n${FILES}\n" | \
 		tee -a "$SRCDIR"/abqaerr.log
--- qa/post/permissions.sh
+++ qa/post/permissions.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 ##permissions: Check for incorrect permissions.
 ##@copyright GPL-2.0+
-FILES="$(find "$PKGDIR/usr/bin" -type f -not -executable -print)"
+FILES="$(find "$PKGDIR/usr/bin" -type f -not -executable -print 2>/dev/null)"
 if [ ! -z "$FILES" ]; then
 	aberr "QA (E324): non-executable file(s) found in /usr/bin:\n\n${FILES}\n" | \
 		tee -a "$SRCDIR"/abqaerr.log
 fi
 
-FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.so*' -not -executable -print)"
+FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.so*' -not -executable -print 2>/dev/null)"
 if [ ! -z "$FILES" ]; then
 	aberr "QA (E324): non-executable shared object(s) found in /usr/lib:\n\n${FILES}\n" | \
 		tee -a "$SRCDIR"/abqaerr.log
 fi
 
-FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.a' -executable -print)"
+FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.a' -executable -print 2>/dev/null)"
 if [ ! -z "$FILES" ]; then
 	aberr "QA (E324): executable static object(s) found in /usr/lib:\n\n${FILES}\n" | \
 		tee -a "$SRCDIR"/abqaerr.log
 fi
 
-FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.o' -executable -print)"
+FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.o' -executable -print 2>/dev/null)"
 if [ ! -z "$FILES" ]; then
 	aberr "QA (E324): executable binary object(s) found in /usr/lib:\n\n${FILES}\n" | \
 		tee -a "$SRCDIR"/abqaerr.log
