e80ba83917e9448c4efc2cefce42b7fb5b3c4ca6
Mingcong Bai <jeffbai@aosc.xyz>
2021-04-12 10:33:50 -0700

    qa/post/permissions: fix .so matching pattern

--- qa/post/permissions.sh
+++ qa/post/permissions.sh
@@ -7,7 +7,7 @@ if [ ! -z "$FILES" ]; then
 		tee -a "$SRCDIR"/abqaerr.log
 fi
 
-FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.so*' -not -executable -print 2>/dev/null)"
+FILES="$(find "$PKGDIR/usr/lib" -type f -name '*.so.*' -not -executable -print 2>/dev/null)"
 if [ ! -z "$FILES" ]; then
 	aberr "QA (E324): non-executable shared object(s) found in /usr/lib:\n\n${FILES}\n" | \
 		tee -a "$SRCDIR"/abqaerr.log
