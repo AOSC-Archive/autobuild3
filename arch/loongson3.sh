582942d04cf5838f4919bf54ef3aac0e631b069e
Mingcong Bai <jeffbai@aosc.xyz>
2021-04-22 16:41:35 -0700

    arch/loongson3: limit LTO quirk to GCC

--- arch/loongson3.sh
+++ arch/loongson3.sh
@@ -3,5 +3,5 @@
 ##@copyright GPL-2.0+
 CFLAGS_GCC_ARCH='-mabi=64 -march=gs464 -mtune=gs464e -mfix-loongson3-llsc -mxgot -flto-partition=none '
 CFLAGS_CLANG_ARCH='-mabi=64 -march=mips64r2 -mtune=mips64r2 '
-LDFLAGS_COMMON_OPTI_LTO="${LDFLAGS_COMMON_OPTI_LTO} -mxgot -flto-partition=none "
+LDFLAGS_GCC_OPTI_LTO="${LDFLAGS_COMMON_OPTI_LTO} -mxgot -flto-partition=none "
 RUSTFLAGS_COMMON_OPTI_LTO="${RUSTFLAGS_COMMON_OPTI_LTO} -Clink-arg=-Wl,-build-id=sha1 -Clink-arg=-Wl,-z,notext "
