aaaf5b39f3b6b7dbf243f6b31062753c951291c2
xyq1113723547 <53088781+xyq1113723547@users.noreply.github.com>
2021-06-12 23:11:58 +0400

    arch/loongarch64: delete  mfix-loongson3-llsc flag

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -2,4 +2,4 @@
 ##arch/loongarch64.sh: Build definitions for Loongson 3A/B 5000.
 ##@copyright GPL-2.0+
 
-CFLAGS_GCC_ARCH='-mabi=lp64 -march=loongarch64 -mtune=loongarch64 -mfix-loongson3-llsc'
+CFLAGS_GCC_ARCH='-mabi=lp64 -march=loongarch64 -mtune=loongarch64'
