b7c5222388ca2cf976e931ed10f3392471949bb5
Mingcong Bai <jeffbai@aosc.xyz>
2021-06-22 08:55:03 -0700

    meta: lint aberr usage, aberr => abdie

--- proc/80-pm_pack.sh
+++ proc/80-pm_pack.sh
@@ -3,5 +3,5 @@
 ##@copyright GPL-2.0+
 for i in $ABMPM $ABAPMS; do
 	abinfo "Building $i package(s) ..."
-	. "$AB"/pm/"$i"/pack || aberr $i packing returned $?.
+	. "$AB"/pm/"$i"/pack || abdie "Failed to build $i package: $?."
 done
