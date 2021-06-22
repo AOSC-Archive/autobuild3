47c7f3c9bd0ce476441b7be586452c6376aa44b6
Mingcong Bai <jeffbai@aosc.xyz>
2021-06-22 08:55:26 -0700

    arch/i386: drop deprecated cross-only architecture definitions

--- arch/i386.sh
+++ arch/i386.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##arch/i386.sh: Build definitions for i386 (subsystem only).
 ##@copyright GPL-2.0+
-[[ $ABBUILD != $ABHOST ]] || aberr "The i386 arch is cross-only, for multilib purposes."
+[[ $ABBUILD != $ABHOST ]] || abdie "The i386 arch is cross-only, for multilib purposes."
 CFLAGS_COMMON_ARCH='-fomit-frame-pointer -march=pentium4 -mtune=core2 -msse -msse2 -msse3 '
 HOSTTOOLPREFIX=/opt/32/bin/i686-pc-linux-gnu
 export PKG_CONFIG_DIR=/opt/32/lib/pkgconfig
