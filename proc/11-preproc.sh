1f1e148acd14a7c0f8b8f9f64353450080bf27bb
Mingcong Bai <jeffbai@aosc.xyz>
2021-07-05 09:41:39 -0700

    proc/11-preproc.sh: use abinfo

--- proc/11-preproc.sh
+++ proc/11-preproc.sh
@@ -3,11 +3,11 @@
 ##@copyright GPL-2.0+
 
 if (( EUID == 0 )); then
-	# Fixing up ownership in "$SRCDIR"
+	abinfo 'Fixing up ownership in $SRCDIR ...'
 	chown -R 0:0 "$SRCDIR" \
 		|| echo "Cannot change source ownership to root:root: $?."
 
-	# Removing SGID bit from "$SRCDIR"
+	abinfo 'Removing SGID bit from $SRCDIR ...'
 	chmod a-s "$SRCDIR" \
 		|| echo "Failed to remove SGID bit: $?."
 fi
