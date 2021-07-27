2c72c5af67ee82b434c33dea84ac18425d877979
Mingcong Bai <jeffbai@aosc.xyz>
2021-07-26 19:45:37 -0700

    qa/pre/sections: also test if PKGSEC was defined

--- qa/pre/sections.sh
+++ qa/pre/sections.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ##sections.sh: Kick the bucket if the section looks bad
 ##@copyright GPL-2.0+
+(test -z $PKGSEC && \
+	aberr 'QA (E104): $PKGSEC not defined.') | \
+		tee -a "$SRCDIR"/abqaerr.log
+
 (grep -qF "$PKGSEC" "$AB/sets/section" || \
 	aberr "QA (E104): $PKGSEC not in sets/section.") | \
 		tee -a "$SRCDIR"/abqaerr.log
