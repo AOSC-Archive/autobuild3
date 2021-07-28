e32badc803ac60b10d04c862f6109ff7be4fbb0e
Mingcong Bai <jeffbai@aosc.xyz>
2021-07-27 23:31:42 -0700

    arch/loongson2f: append -flto-partition=none to fix LTO

--- arch/loongson2f.sh
+++ arch/loongson2f.sh
@@ -7,3 +7,6 @@ CFLAGS_COMMON_OPTI='-Os '
 # Retro: Keeping -ftree-vectorization for MMI (effective?).
 
 CFLAGS_COMMON_ARCH='-mabi=64 -march=mips3 -mtune=loongson2f -mloongson-mmi -Wa,-mfix-loongson2f-nop '
+
+CFLAGS_GCC_OPTI_LTO="${CFLAGS_COMMON_OPTI_LTO} -flto-partition=none "
+LDFLAGS_GCC_OPTI_LTO="${LDFLAGS_COMMON_OPTI_LTO} -mxgot -flto-partition=none "
