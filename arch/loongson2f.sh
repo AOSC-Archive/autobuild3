0608c7c04a4b48d5a06f74dcdbbe12b6a111d426
Mingcong Bai <jeffbai@aosc.xyz>
2021-08-01 18:05:12 -0700

    arch/loongson2f: mark -mloongson-mmi, -mfix-loongson2f-nop as GCC-specific

--- arch/loongson2f.sh
+++ arch/loongson2f.sh
@@ -6,7 +6,8 @@
 CFLAGS_COMMON_OPTI='-Os '
 # Retro: Keeping -ftree-vectorization for MMI (effective?).
 
-CFLAGS_COMMON_ARCH='-mabi=64 -march=mips3 -mtune=loongson2f -mloongson-mmi -Wa,-mfix-loongson2f-nop '
+CFLAGS_COMMON_ARCH='-mabi=64 -march=mips3 -mtune=loongson2f '
+CFLAGS_GCC_ARCH='-mloongson-mmi -Wa,-mfix-loongson2f-nop '
 
 CFLAGS_GCC_OPTI_LTO="${CFLAGS_COMMON_OPTI_LTO} -flto-partition=none "
 LDFLAGS_GCC_OPTI_LTO="${LDFLAGS_COMMON_OPTI_LTO} -mxgot -flto-partition=none "
