9557c950fbd015c3738d2a048115ba67d25df59d
Mingcong Bai <jeffbai@aosc.xyz>
2021-10-01 13:45:08 -0700

    proc/40-patch: add rpatch/rdiff routine; add return code to error message

--- proc/40-patch.sh
+++ proc/40-patch.sh
@@ -4,6 +4,7 @@
 abrequire arch
 
 PATCHFLAGS="-Np1 -t"
+RPATCHFLAGS="-Rp1 -t"
 
 if [ ! -f .patch ]
 then
@@ -14,19 +15,27 @@ then
 	then
 		for i in $(grep -v '^#' autobuild/patches/series); do
 			abinfo "Applying patch $i ..."
-			patch $PATCHFLAGS -i "autobuild/patches/$i" || abdie "Applying patch $i failed"
+			patch $PATCHFLAGS -i "autobuild/patches/$i" || abdie "Applying patch $i failed: $?."
 		done
 		touch .patch
 	elif [ -d autobuild/patches ]
 	then
 		for i in autobuild/patches/*.{patch,diff}; do
 			abinfo "Applying patch $i ..."
-			patch $PATCHFLAGS -i "$i" || abdie "Applying patch $i failed"
+			patch $PATCHFLAGS -i "$i" || abdie "Applying patch $i failed: $?."
 		done
 		for i in autobuild/patches/*.{patch,diff}."${CROSS:-$ARCH}"; do
 			abinfo "Applying patch $i (for ${CROSS:-$ARCH}) ..."
 			patch $PATCHFLAGS -i "$i" || abdie "Applying patch $i for ${CROSS:-$ARCH} failed: $?."
 		done
-		touch .patch
+		for i in autobuild/patches/*.r{patch,diff}; do
+			abinfo "Reverting patch $i ..."
+			patch $RPATCHFLAGS -i "$i" || abdie "Reverting patch $i failed: $?."
+		done
+		for i in autobuild/patches/*.r{patch,diff}."${CROSS:-$ARCH}"; do
+			abinfo "Applying patch $i (for ${CROSS:-$ARCH}) ..."
+			patch $RPATCHFLAGS -i "$i" || abdie "Reverting patch $i for ${CROSS:-$ARCH} failed: $?."
+		done
+		touch "$SRCDIR"/.patch
 	fi
 fi
