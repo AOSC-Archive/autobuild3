c46957167a136747befed29fcd539eb5a59ce636
Mingcong Bai <jeffbai@aosc.xyz>
2022-01-13 11:12:21 -0800

    proc/35-build_env_src: add quirk for /etc/profile.d/30-nvm.sh
    This is a (hopefully) temporary workaround for a mysterious failure when
    Autobuild3 attempts to source /etc/profile.d/30-nvm.sh whilst runnning
    proc/35-build_env_src.

--- proc/35-build_env_src.sh
+++ proc/35-build_env_src.sh
@@ -2,6 +2,12 @@
 ##proc/build_env_src: Source build environment definition scripts.
 ##@copyright GPL-2.0+
 for i in /etc/profile.d/*.sh; do
-    abinfo "Sourcing environment definition script $i ..."
-    source "$i" || abdie "Error sourcing environment definition: $i."
+        abinfo "Sourcing environment definition script $i ..."
+	# FIXME: A quirk for 30-nvm.sh, which mysteriously fails as Autobuild3 sources it.
+	if [[ "$i" != "/etc/profile.d/30-nvm.sh" ]]; then
+		source "$i" || abdie "Error sourcing environment definition: $i."
+	else
+		abwarn "FIXME: Ignoring errors for $i ..."
+		source "$i" || true
+	fi
 done
