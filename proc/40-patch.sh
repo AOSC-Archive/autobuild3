AOSC-Archive/autobuild3

commit 340a85fca1e66f6f6db526c33b9d206c648d1c61

proc/40-patch: adapt patches/series to ab_apply_patches
- Also make sure the script uses absolute paths.

proc/40-patch.sh
@@ -5,17 +5,19 @@ abrequire arch
 
 if [ ! -f .patch ]
 then
-	if arch_loadfile_strict patch
-	then
-		touch .patch
-	elif [ -f autobuild/patches/series ]
-	then
-		ab_apply_patches $(grep -v '^#' autobuild/patches/series)
-		touch .patch
-	elif [ -d autobuild/patches ]
-	then
-		ab_apply_patches autobuild/patches/*.{patch,diff} autobuild/patches/*.{patch,diff}."${CROSS:-$ARCH}"
-		ab_reverse_patches autobuild/patches/r*.{patch,diff} autobuild/patches/*.r{patch,diff}."${CROSS:-$ARCH}"
+	if arch_loadfile_strict patch; then
+		touch "$SRCDIR"/.patch
+	elif [ -f "$SRCDIR"/autobuild/patches/series ]; then
+		ab_apply_patches \
+			"$SRCDIR"/autobuild/patches/$(grep -v '^#' autobuild/patches/series)
+		touch "$SRCDIR"/.patch
+	elif [ -d "$SRCDIR"/autobuild/patches ]; then
+		ab_apply_patches \
+			"$SRCDIR"/autobuild/patches/*.{patch,diff} \
+			"$SRCDIR"/autobuild/patches/*.{patch,diff}."${CROSS:-$ARCH}"
+		ab_reverse_patches \
+			"$SRCDIR"/autobuild/patches/r*.{patch,diff} \
+			"$SRCDIR"/autobuild/patches/*.r{patch,diff}."${CROSS:-$ARCH}"
 		touch "$SRCDIR"/.patch
 	fi
 fi

commit 0be925e1334275ece7814a8fb79a5f24c4d9fc90

proc/40-patch: fix regression of series file
Currently patch files defined by series file are not applied correctly
(only the first one is applied).

Fix this.

Fixes: 340a85fca1e6 ("proc/40-patch: adapt patches/series to ab_apply_patches")
Fixes: 1a4dfcf824b6 ("lib/builtins: add helper functions ...")
Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

proc/40-patch.sh
@@ -9,7 +9,7 @@ then
 		touch "$SRCDIR"/.patch
 	elif [ -f "$SRCDIR"/autobuild/patches/series ]; then
 		ab_apply_patches \
-			"$SRCDIR"/autobuild/patches/$(grep -v '^#' autobuild/patches/series)
+			$(grep -v '^#' autobuild/patches/series | sed "s@^@$SRCDIR/autobuild/patches/@")
 		touch "$SRCDIR"/.patch
 	elif [ -d "$SRCDIR"/autobuild/patches ]; then
 		ab_apply_patches \