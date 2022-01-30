a348cb317f9386128ef4fcba2b134161c8f3fe10
Icenowy Zheng <icenowy@aosc.io>
2022-01-31 07:03:41 +0800

    arch/riscv64: use GNU ld.bfd for Rust LTO now
    LLD is not ready for generic-purpose RV Linux distros yet.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- arch/riscv64.sh
+++ arch/riscv64.sh
@@ -2,3 +2,5 @@
 ##arch/riscv64.sh: Build definitions for riscv64.
 ##@copyright GPL-2.0+
 LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/riscv64/usr/lib -L/var/ab/cross-root/riscv64/usr/lib '
+# LLD does not support RISC-V relax now.
+RUSTFLAGS_COMMON_OPTI_LTO='-Clink-arg=-fuse-ld=bfd -Clink-arg=-Wl,-build-id=sha1'
