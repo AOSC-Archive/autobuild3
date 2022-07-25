8d6c0fa098fdeda8f73e73074093d8cb17937023
Mingcong Bai <jeffbai@aosc.xyz>
2022-07-24 17:16:13 -0700

    filter/retro_drop_docs: use ab_match_archgroup

--- filter/retro_drop_docs.sh
+++ filter/retro_drop_docs.sh
@@ -2,13 +2,7 @@
 ##filter/retro_drop_docs.sh: Drop /usr/share/{,gtk-}doc for Retro architectures.
 ##@copyright GPL-2.0+
 filter_retro_drop_docs() {
-	if [[ "${CROSS:-$ARCH}" = "armv4" || \
-	      "${CROSS:-$ARCH}" = "armv6hf" || \
-	      "${CROSS:-$ARCH}" = "armv7hf" || \
-	      "${CROSS:-$ARCH}" = "i486" || \
-	      "${CROSS:-$ARCH}" = "loongson2f" || \
-	      "${CROSS:-$ARCH}" = "powerpc" || \
-	      "${CROSS:-$ARCH}" = "ppc64" ]]; then
+	if ab_match_archgroup retro; then
 		if [ -d "$PKGDIR"/usr/share/doc ]; then
 			abinfo "Dropping documentation ..."
 			rm -r "$PKGDIR"/usr/share/doc
