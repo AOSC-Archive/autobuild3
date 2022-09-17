6f9c81f9eb0fa9779f04acce99037ebdf664c635
Camber Huang <camber@aosc.io>
2022-09-18 03:25:50 +0800

    scriptlet/01-alternatives.sh: bugfix and enhancements (#136)
    * Fixes issue #136
    * Now supports group-specific alternatives file, like arch_loadvar()
    ... in lib/arch.sh
    * More abinfo/abdbg outputs!
    
    Signed-off-by: Camber Huang <camber@aosc.io>

--- scriptlet/01-alternatives.sh
+++ scriptlet/01-alternatives.sh
@@ -13,18 +13,45 @@ addalt(){
 # alternative path link prio [path2 link2 prio2 ..]
 alternative(){ while (($#)); do addalt "$1" "$(basename "$1")" "$2" "$3"; shift 3 || break; done; }
 
-if [ -e "$SRCDIR"/autobuild/${CROSS:-$ARCH}/alternatives ]; then
-	echo "#>start 01-alternatives" >> abscripts/postinst
-	echo "#>start 01-alternatives" >> abscripts/prerm
-	. "$SRCDIR"/autobuild/${CROSS:-$ARCH}/alternatives
-	echo "#>end 01-alternatives" >> abscripts/postinst
-	echo "#>end 01-alternatives" >> abscripts/prerm
-elif [ -e "$SRCDIR"/autobuild/${CROSS:-$ARCH}/alternatives ]; then
-	echo "#>start 01-alternatives" >> abscripts/postinst
-	echo "#>start 01-alternatives" >> abscripts/prerm
-	. "$SRCDIR"/autobuild/alternatives
-	echo "#>end 01-alternatives" >> abscripts/postinst
-	echo "#>end 01-alternatives" >> abscripts/prerm
+_hasAlternative=0
+
+if [ -e "$SRCDIR"/autobuild/alternatives ]; then
+	abdbg "Found common alternatives file: ${SRCDIR}/autobuild/alternatives"
+	_hasAlternative=1
 fi
 
-unset alternative addalt
+## ${CROSS:-$ARCH} or $ABHOST is always prior than arch groups
+if [ -e "$SRCDIR"/autobuild/${CROSS:-$ARCH}/alternatives ]; then
+	abdbg "Found arch-specific alternatives file: ${SRCDIR}/autobuild/${CROSS:-$ARCH}/alternatives"
+	_hasAlternative=1
+	_archTarget="/${CROSS:-$ARCH}"
+else
+	for _grp in ${ABHOST_GROUP}; do
+		if [ -e "$SRCDIR"/autobuild/${_grp}/alternatives ]; then
+			if [ $_assignedGroup ]; then
+				aberr "Refusing to treat autobuild/${_grp}/alternatives as the group-specific alternative file"
+				aberr "... because there is another file: autobuild/${_assignedGroup}/alternatives"
+				abinfo "Current ABHOST ${ABHOST} belongs to the following groups:"
+				abinfo "${ABHOST_GROUP//$'\n'/, }"
+				abinfo "Create autobuild/${CROSS:-$ARCH}/alternatives instead to suppress the conflict"
+				abdie "Ambiguous group-specific alternative file detected! Refuse to proceed."
+			fi
+			abdbg "Found arch-group-specific alternatives file: ${SRCDIR}/autobuild/${_grp}/alternatives"
+			_hasAlternative=1
+			_archTarget="/${_grp}"
+			_assignedGroup=${_grp}
+		fi
+	done
+fi
+if bool "$_hasAlternative"; then
+	abinfo "Taking alternatives file: ${SRCDIR}/autobuild${_archTarget}/alternatives"
+	echo "#>start 01-alternatives" >> abscripts/postinst
+	echo "#>start 01-alternatives" >> abscripts/prerm
+	. "$SRCDIR"/autobuild"$_archTarget"/alternatives
+	echo "#>end 01-alternatives" >> abscripts/postinst
+	echo "#>end 01-alternatives" >> abscripts/prerm
+else
+	abinfo "No alternatives file found."
+fi
+
+unset alternative addalt _archTarget _assignedGroup _grp _hasAlternative
