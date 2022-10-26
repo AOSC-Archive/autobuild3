66fc9d08f1acd0f3b54ef14e52749183e0682cf1
Mingcong Bai <jeffbai@aosc.xyz>
2022-10-25 23:20:02 -0700

    build/00-self: handle .stage2 suffix

--- build/00-self.sh
+++ build/00-self.sh
@@ -9,8 +9,22 @@ build_self_probe(){
 
 build_self_build(){
 	BUILD_START
-	arch_loadfile_strict build \
-		|| abdie "Failed to run self-defined build script: $?."
+	if ! bool $ABSTAGE2; then
+		arch_loadfile_strict build || \
+			abdie "Failed to run self-defined build script: $?."
+	else
+		if arch_findfile build.stage2; then
+			abwarn "ABSTAGE2 returned true, loading stage2 build script ..."
+			arch_loadfile_strict build.stage2 || \
+				abdie "Failed to run self-defined stage2 build script: $?."
+		else
+			abwarn "ABSTAGE2 returned true, loading stage2 build script ..."
+			abwarn "Could not find self-defined stage2 build script, falling back to normal build script ..."
+			arch_loadfile_strict build || \
+				abdie "Failed to run self-defined build script: $?."
+		fi
+	fi
+
 	cd "$SRCDIR"
 }
 
