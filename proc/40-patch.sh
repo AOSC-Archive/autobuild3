5d362211c94a3e8ec29b8900697839583b535bf1
Mingcong Bai <jeffbai@aosc.xyz>
2022-10-25 23:21:05 -0700

    proc/40-patch: introduce .stage2 suffix support

--- proc/40-patch.sh
+++ proc/40-patch.sh
@@ -3,9 +3,21 @@
 ##@copyright GPL-2.0+
 abrequire arch
 
-if [ ! -f "$SRCDIR"/.patch ]
-then
-	if arch_loadfile_strict patch; then
+if [ ! -f "$SRCDIR"/.patch ]; then
+	if [ -f "$SRCDIR"/autobuild/patch ]; then
+		arch_loadfile_strict patch
+		touch "$SRCDIR"/.patch
+	elif [ -f "$SRCDIR"/autobuild/patch ] && bool $ABSTAGE2; then
+		if arch_loadfile_strict patch.stage2; then
+			abwarn "ABSTAGE2 returned true, running stage2 patch script ..."
+			arch_loadfile_strict patch.stage2
+		elif arch_loadfile_strict patch; then
+			abwarn "ABSTAGE2 returned true, running stage2 patch script ..."
+			abwarn "Could not find self-defined stage patch script, failling back to main patch script ..."
+			arch_loadfile_strict patch
+		else
+			true
+		fi
 		touch "$SRCDIR"/.patch
 	elif [ -f "$SRCDIR"/autobuild/patches/series ]; then
 		ab_apply_patches \
