cebfb43c985ef5c770316bb8d5bcbbc607a52044
Icenowy Zheng <uwu@icenowy.me>
2022-12-05 21:49:43 +0800

    arch/loongarch64.sh: default to lp64d
    This is the ABI used by CLFS system by Sunhaiyong, [1] and it should also be
    the best one here because we're not going to support any FPU-less
    LoongArch systems.
    
    [1] https://github.com/sunhaiyong1978/CLFS-for-LoongArch/blob/main/CLFS_For_LoongArch64.md#%E8%AE%BE%E7%BD%AE%E5%88%B6%E4%BD%9C%E7%94%A8%E6%88%B7%E7%8E%AF%E5%A2%83
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -2,7 +2,7 @@
 ##arch/loongarch64.sh: Build definitions for Loongson 3A/B 5000.
 ##@copyright GPL-2.0+
 
-CFLAGS_GCC_ARCH='-mabi=lp64 -march=loongarch64 -mtune=loongarch64'
+CFLAGS_GCC_ARCH='-mabi=lp64d -march=loongarch64 -mtune=loongarch64'
 
 # Position-independent executable buildmode is not available on loong64 architecture.
 # Removing for loongarch64 target.
