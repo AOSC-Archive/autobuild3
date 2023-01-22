ecc0274cd8e940fe003547238c952e21357dc094
liushuyu <liushuyu011@gmail.com>
2023-01-21 17:24:50 -0700

    arch/_common_switches: remove -spec switches from Clang flags

--- arch/_common_switches.sh
+++ arch/_common_switches.sh
@@ -6,7 +6,7 @@ if ((AB_FLAGS_SCP)); then CFLAGS_GCC_COMMON+=' -fstack-clash-protection '; fi
 if ((AB_FLAGS_RRO)); then LDFLAGS_COMMON+=' -Wl,-z,relro '; fi
 if ((AB_FLAGS_NOW)); then LDFLAGS_COMMON+=' -Wl,-z,now '; fi
 if ((AB_FLAGS_FTF)); then CPPFLAGS_COMMON+=' -D_FORTIFY_SOURCE=2 '; fi
-if ((AB_FLAGS_SPECS)); then LDFLAGS_COMMON+=" -specs=/usr/lib/gcc/specs/hardened-ld "; CFLAGS_GCC_OPTI+=" -specs=/usr/lib/gcc/specs/hardened-cc1 "; fi
+if ((AB_FLAGS_SPECS)); then CFLAGS_GCC_OPTI+=" -specs=/usr/lib/gcc/specs/hardened-cc1 "; fi
 if ((AB_FLAGS_O3)); then CFLAGS_COMMON_OPTI="${CFLAGS_COMMON_OPTI/O2/O3}"; fi
 if ((AB_FLAGS_OS)); then CFLAGS_COMMON_OPTI="${CFLAGS_COMMON_OPTI/O2/Os}"; fi
 if ((AB_FLAGS_EXC)); then CFLAGS_COMMON+=' -fexceptions '; fi
@@ -15,6 +15,7 @@ if ((AB_FLAGS_EXC)); then CFLAGS_COMMON+=' -fexceptions '; fi
 if bool $USECLANG; then
 	if ((AB_FLAGS_PIC)); then LDFLAGS_COMMON+=' -fPIC ' CFLAGS_COMMON+=' -fPIC '; fi
 	if ((AB_FLAGS_PIE)); then LDFLAGS_COMMON+=' -fPIE -pie ' CFLAGS_COMMON+=' -fPIE '; fi
+elif ((AB_FLAGS_SPECS)); then LDFLAGS_COMMON+=" -specs=/usr/lib/gcc/specs/hardened-ld ";
 fi
 
 if bool $ABSPLITDBG; then
