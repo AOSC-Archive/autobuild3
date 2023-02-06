0d2c2c617101d558e640432c979d4a4fab6cd265
Mingcong Bai <jeffbai@aosc.xyz>
2023-02-06 12:18:59 -0800

    arch/ppc64el.sh: tweak flags
    - Drop all POWER8-specific flags.
    - Set ieeelongdouble as the default ABI.

--- arch/ppc64el.sh
+++ arch/ppc64el.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ##arch/ppc64el.sh: Build definitions for ppc64el.
 ##@copyright GPL-2.0+
-CFLAGS_GCC_ARCH='-mcpu=power8 -mtune=power9 -mpower8-vector -mpower8-fusion -maltivec -mabi=altivec -msecure-plt -mvsx'
-CFLAGS_CLANG_ARCH='-mcpu=power8 -mtune=power9 -mpower8-vector -maltivec -mabi=altivec -msecure-plt -mvsx'
+CFLAGS_GCC_ARCH='-mcpu=power8 -mtune=power9 -mabi=altivec -msecure-plt -mvsx -mabi=ieeelongdouble'
+CFLAGS_CLANG_ARCH='-mcpu=power8 -mtune=power9 -mabi=altivec -msecure-plt -mvsx -mabi=ieeelongdouble'
