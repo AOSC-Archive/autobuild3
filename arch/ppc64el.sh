27eab36016e9cd81772db2da5b5ca1e8ae843fee
Mingcong Bai <jeffbai@aosc.xyz>
2023-02-06 12:13:30 -0800

    arch/ppc64el.sh: drop -DNO_WARN_X86_INTRINSICS
    It's a flag with unpredictable implications when enabled globally, in the
    future, only use it for packages that need this specific flag.

--- arch/ppc64el.sh
+++ arch/ppc64el.sh
@@ -3,4 +3,3 @@
 ##@copyright GPL-2.0+
 CFLAGS_GCC_ARCH='-mcpu=power8 -mtune=power9 -mpower8-vector -mpower8-fusion -maltivec -mabi=altivec -msecure-plt -mvsx'
 CFLAGS_CLANG_ARCH='-mcpu=power8 -mtune=power9 -mpower8-vector -maltivec -mabi=altivec -msecure-plt -mvsx'
-CPPFLAGS_COMMON_ARCH='-DNO_WARN_X86_INTRINSICS '
