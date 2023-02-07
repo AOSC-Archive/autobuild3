55c7eaba0a1df9869e4fc18cf0edb9c84549394b
Mingcong Bai <jeffbai@aosc.xyz>
2023-02-06 22:09:03 -0800

    arch/ppc64el.sh: drop overriden -mabi=altivec

--- arch/ppc64el.sh
+++ arch/ppc64el.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ##arch/ppc64el.sh: Build definitions for ppc64el.
 ##@copyright GPL-2.0+
-CFLAGS_GCC_ARCH='-mcpu=power8 -mtune=power9 -mabi=altivec -msecure-plt -mvsx -mabi=ieeelongdouble'
-CFLAGS_CLANG_ARCH='-mcpu=power8 -mtune=power9 -mabi=altivec -msecure-plt -mvsx -mabi=ieeelongdouble'
+CFLAGS_GCC_ARCH='-mcpu=power8 -mtune=power9 -msecure-plt -mvsx -mabi=ieeelongdouble'
+CFLAGS_CLANG_ARCH='-mcpu=power8 -mtune=power9 -msecure-plt -mvsx -mabi=ieeelongdouble'
