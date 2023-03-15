e402417ec6877d53f4441f130b7e7917e75418e9
Mingcong Bai <jeffbai@aosc.xyz>
2023-03-15 01:12:59 -0700

    pm/dpkg/lib.sh: implement Omakase (oma) support
    Fallback to apt-get (original behavior) if the oma command is not found.

--- pm/dpkg/lib.sh
+++ pm/dpkg/lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##dpkg/lib.sh: What dpkg should provide to the abstraction as a package manager
 ##@copyright GPL-2.0+
-abreqexe dpkg dpkg-deb dpkg-query apt-get
+abreqexe dpkg dpkg-deb dpkg-query oma apt-get
 
 # $1 is a list of files. Do not quote.
 pm_whoprov(){
@@ -20,11 +20,27 @@ pm_exists(){
 }
 
 pm_repoupdate(){
-	apt-get update
+	if command -v oma > /dev/null; then
+		abinfo "Omakase (oma) detected, using oma ..."
+		oma refresh
+	elif command -v apt-get > /dev/null; then
+		abwarn "Omakase (oma) not found, using apt-get as fallback ..."
+		apt-get update
+	else
+		aberr "No usable dpkg package management frontend!"
+	fi
 }
 
 pm_repoinstall(){
-	apt-get install "$@" --yes
+	if command -v oma > /dev/null; then
+		abinfo "Omakase (oma) detected, using oma ..."
+		oma install "$@" -y --force-confnew
+	elif command -v apt > /dev/null; then
+		abwarn "Omakase (oma) not found, using apt-get as fallback ..."
+		apt-get install "$@" --yes
+	else
+		aberr "No usable dpkg package management frontend!"
+	fi
 }
 
 pm_chroot(){
