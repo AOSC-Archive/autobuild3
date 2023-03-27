21f6446f910601e4593e5c924ffbd255e4efa91b
liushuyu <liushuyu011@gmail.com>
2023-03-26 22:09:50 -0600

    arch/riscv64.sh: force enable D extension in LTO pass in lld

--- arch/riscv64.sh
+++ arch/riscv64.sh
@@ -2,3 +2,4 @@
 ##arch/riscv64.sh: Build definitions for riscv64.
 ##@copyright GPL-2.0+
 LDFLAGS_COMMON_CROSS='-Wl,-rpath -Wl,/usr/lib -Wl,-rpath-link -Wl,/var/ab/cross-root/riscv64/usr/lib -L/var/ab/cross-root/riscv64/usr/lib '
+RUSTFLAGS_COMMON_OPTI_LTO="${RUSTFLAGS_COMMON_OPTI_LTO} -Clink-arg=-Wl,-mllvm=-mattr=+d -Clink-arg=-mabi=lp64d"
