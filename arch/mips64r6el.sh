c7b0513186930043237a907bdaf47a925a85b30a
Cyan <cyanoxygen@aosc.io>
2023-03-31 10:05:32 +0800

    arch/mips64r6el: apply Rust workarounds for mips64r6el
    - Add `-Cdebuginfo=0` to RUSTFLAGS, to workaround crashing of LLVM when
      debug info generation turned on, by forcibly disabling debug info
      generation.
    - Add `--cfg=rustix_use_libc` to RUSTFLAGS, to workaround generation of
      mips64el r2 binaries for rustix, since rustix treats mips64r6el as
      mips64[r2]el.

--- arch/mips64r6el.sh
+++ arch/mips64r6el.sh
@@ -2,4 +2,4 @@
 ##arch/mips64r6el.sh: Build definitions for mips64r6el.
 ##@copyright GPL-2.0+
 CFLAGS_COMMON_ARCH='-march=mips64r6 -mtune=mips64r6 -mcompact-branches=always -mmsa '
-RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=mips64r6 '
+RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=mips64r6 -Cdebuginfo=0 --cfg=rustix_use_libc '
