d9b6fab956271ad06134c893e71ebd467d127c8f
liushuyu <liushuyu011@gmail.com>
2023-04-06 23:14:19 -0600

    arch/_common.sh: use more aggressive LTO flags for Rust ...
    ... this somehow fixes the linkage issue on loongson3

--- arch/_common.sh
+++ arch/_common.sh
@@ -52,7 +52,7 @@ RUSTFLAGS_COMMON=''
 RUSTFLAGS_COMMON_OPTI='-Ccodegen-units=1 -Copt-level=3 '
 RUSTFLAGS_COMMON_WEIRD=''
 # Use clang + lld for processing LTO
-RUSTFLAGS_COMMON_OPTI_LTO='-Clinker-plugin-lto -Clinker=clang -Clink-arg=-flto -Clink-arg=-fuse-ld=lld -Clink-arg=-Wl,-build-id=sha1 -Clink-arg=-Wl,-z,notext'
+RUSTFLAGS_COMMON_OPTI_LTO='-Cembed-bitcode=yes -Clto -Clinker-plugin-lto -Clinker=clang -Clink-arg=-flto -Clink-arg=-fuse-ld=lld -Clink-arg=-Wl,-build-id=sha1 -Clink-arg=-Wl,-z,notext'
 # Linker Flags. (actually passed to your CC, just FYI)
 # LDFLAGS writing helpers:
 ld_arg(){ printf %s '-Wl'; local IFS=,; printf %s "$*"; }
