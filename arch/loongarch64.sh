fa93bb25547202ac861759b64ade958f6499bbb5
Mingcong Bai <jeffbai@aosc.xyz>
2023-04-09 11:30:38 -0700

    arch/loongarch64.sh: adapt for newer toolchain
    - Rename processor target, loongarch64 => la464.
    - Drop Golang PIE hack.

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -2,8 +2,4 @@
 ##arch/loongarch64.sh: Build definitions for Loongson 3A/B 5000.
 ##@copyright GPL-2.0+
 
-CFLAGS_GCC_ARCH='-mabi=lp64d -march=loongarch64 -mtune=loongarch64'
-
-# Position-independent executable buildmode is not available on loong64 architecture.
-# Removing for loongarch64 target.
-GOFLAGS=${GOFLAGS/-buildmode=pie/}
+CFLAGS_GCC_ARCH='-mabi=lp64d -march=la464 -mtune=la464'
