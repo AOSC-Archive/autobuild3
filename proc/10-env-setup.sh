d3a03dea927092935ec8ef46c7738b08b5a8e72a
Mingcong Bai <jeffbai@aosc.xyz>
2023-05-07 00:46:07 -0700

    proc/10-env-setup.sh: preload hook_uname.so when detected
    This will help fix a specific machine name that matches our architecture specs.

--- proc/10-env-setup.sh
+++ proc/10-env-setup.sh
@@ -7,3 +7,8 @@ export SHELL="/bin/bash"
 
 abinfo 'Setting $HOME to /root ...'
 export HOME="/root"
+
+if [ -e /usr/lib/hook_uname.so ]; then
+    abinfo 'hook_uname.so detected, preloading ...'
+    export LD_PRELOAD="/usr/lib/hook_uname.so:$LD_PRELOAD"
+fi
