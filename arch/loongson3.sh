290c1f31d09189b3ef33f3065e7d4089848f40a8
liushuyu <liushuyu011@gmail.com>
2023-05-26 13:08:24 -0600

    arch: use RUSTFLAGS_COMMON_ARCH to replace ...
    ... RUSTFLAGS_COMMON_OPTI_LTO

--- arch/loongson3.sh
+++ arch/loongson3.sh
@@ -3,9 +3,9 @@
 ##@copyright GPL-2.0+
 CFLAGS_GCC_ARCH='-mabi=64 -march=gs464 -mtune=gs464e -mfix-loongson3-llsc -mxgot '
 CFLAGS_CLANG_ARCH='-mabi=64 -march=mips64r2 -mtune=mips64r2 '
+RUSTFLAGS_COMMON_ARCH='-Clink-arg=-Wl,-build-id=sha1 -Clink-arg=-Wl,-z,notext '
 CFLAGS_GCC_OPTI_LTO="${CFLAGS_COMMON_OPTI_LTO} -flto-partition=none "
 LDFLAGS_GCC_OPTI_LTO="${LDFLAGS_COMMON_OPTI_LTO} -mxgot -flto-partition=none "
-RUSTFLAGS_COMMON_OPTI_LTO="${RUSTFLAGS_COMMON_OPTI_LTO} -Clink-arg=-Wl,-build-id=sha1 -Clink-arg=-Wl,-z,notext "
 
 # Position-independent executable buildmode is not available on any mips architecture.
 # Removing for loongson3 target.
