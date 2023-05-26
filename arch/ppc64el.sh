a7ca60392634b20f179abce9854955e068d9d450
liushuyu <liushuyu011@gmail.com>
2023-05-26 13:09:50 -0600

    arch/ppc64el.sh: add new RUSTFLAGS ...
    * Enables vsx, altivec, and secure-plt.
    * Checks for ieeelongdouble during linking.
    * Targets POWER 8 processor

--- arch/ppc64el.sh
+++ arch/ppc64el.sh
@@ -3,3 +3,4 @@
 ##@copyright GPL-2.0+
 CFLAGS_GCC_ARCH='-mcpu=power8 -mtune=power9 -msecure-plt -mvsx -mabi=ieeelongdouble'
 CFLAGS_CLANG_ARCH='-mcpu=power8 -mtune=power9 -msecure-plt -mvsx -mabi=ieeelongdouble'
+RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=pwr8 -Ctarget-feature=+vsx,+altivec,+secure-plt -Clink-arg=-mabi=ieeelongdouble'
