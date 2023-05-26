24382c890a01f2274b33a733fd5a76c2043b3e9f
liushuyu <liushuyu011@gmail.com>
2023-05-26 14:28:48 -0600

    arch/ppc64.sh: match RUSTFLAGS with CFLAGS ...
    * Enable AltiVec, secure-plt, and hard float.
    * Use 64-bit instructions.
    * Targets G5 CPU

--- arch/ppc64.sh
+++ arch/ppc64.sh
@@ -10,6 +10,7 @@ CFLAGS_COMMON_ARCH=' -m64 -mcpu=G5 -maltivec -mabi=altivec -msecure-plt -mhard-f
 CFLAGS_GCC_OPTI_LTO="${CFLAGS_COMMON_OPTI_LTO} -flto-partition=none "
 LDFLAGS_GCC_OPTI_LTO="${LDFLAGS_COMMON_OPTI_LTO} -flto-partition=none "
 
+RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=g5 -Ctarget-features=+altivec,+secure-plt,+hard-float,+64bit '
 # LLD does not support POWER ABI v1.
 RUSTFLAGS_COMMON_OPTI_LTO='-Clink-arg=-fuse-ld=bfd -Clink-arg=-Wl,-build-id=sha1'
 
