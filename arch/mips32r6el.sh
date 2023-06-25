054e7ca512e9563128fea2859593cd897155966d
Mingcong Bai <jeffbai@aosc.xyz>
2023-06-25 08:57:26 +0800

    arch/mips32r6el.sh: drop -mmsa flag
    This flag generates broken binaries (Glibc segfaults, ncurses [tic] could not
    generate terminfo correctly. Not sure if this is a Qemu issue or not.

--- arch/mips32r6el.sh
+++ arch/mips32r6el.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##arch/mips32r6el.sh: Build definitions for mips32r6el.
 ##@copyright GPL-2.0+
-CFLAGS_COMMON_ARCH='-march=mips32r6 -mtune=mips32r6 -mcompact-branches=always -mmsa '
+CFLAGS_COMMON_ARCH='-march=mips32r6 -mtune=mips32r6 -mcompact-branches=always '
 
 # FIXME: -Cdebuginfo=0 enables rustc to build programs on mips32r6el,
 # overriding all project configuration and build profile settings.
@@ -11,4 +11,4 @@ CFLAGS_COMMON_ARCH='-march=mips32r6 -mtune=mips32r6 -mcompact-branches=always -m
 # rustix contains MIPS32 R2 assembly, without being able to distinguish between
 # R2 and R6 assemblies. Enabling this options instructs rustix to use the libc
 # backend instead.
-RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=mips32r6 -Cdebuginfo=0 -Ctarget-feature=+msa -Cllvm-args=--mips-compact-branches=always --cfg=rustix_use_libc '
+RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=mips32r6 -Cdebuginfo=0 -Cllvm-args=--mips-compact-branches=always --cfg=rustix_use_libc '
