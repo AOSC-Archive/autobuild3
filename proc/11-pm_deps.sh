0a32dba4ed464457e34771ec9c9723aea362f038
Mingcong Bai <jeffbai@aosc.xyz>
2023-09-13 20:48:19 -0700

    proc/11-pm_deps.sh: fix typo bool VER_NONE => bool $VER_NONE

--- proc/11-pm_deps.sh
+++ proc/11-pm_deps.sh
@@ -5,7 +5,7 @@ abrequire pm
 
 # FIXME: The flat stuff gets stupid with 'foo | bar' packs. Guess why.
 if bool $ABBUILDDEPONLY; then
-	if ! bool VER_NONE; then
+	if ! bool $VER_NONE; then
 		abdie "ABBUILDDEPONLY must be used with VER_NONE=1 (dependency version-agnostic). Aborting."
 	fi
 	FLATDEP="$(pm_deflat $BUILDDEP)"
