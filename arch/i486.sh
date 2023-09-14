98136cd020050049ec9c68860149410987d0bcc1
Mingcong Bai <jeffbai@aosc.xyz>
2023-09-13 21:32:28 -0700

    arch/i486.sh: move gc-sections flags to _OPTI

--- arch/i486.sh
+++ arch/i486.sh
@@ -3,11 +3,11 @@
 ##@copyright GPL-2.0+
 
 # Retro: Overriding mainline definitions, and take more interest in reducing code size.
-CFLAGS_COMMON_OPTI='-Os '
+CFLAGS_COMMON_OPTI='-Os -ffunction-sections -fdata-sections '
+LDFLAGS_COMMON_OPTI='-Wl,--gc-sections '
 # Retro: Also disabling -ftree-vectorization which could potentially enlarge code size.
 CFLAGS_GCC_OPTI='-fira-loop-pressure -fira-hoist-pressure '
 
-CFLAGS_COMMON_ARCH='-march=i486 -mtune=bonnell -ffunction-sections -fdata-sections '
-LDFLAGS_COMMON_ARCH='-Wl,--gc-sections '
+CFLAGS_COMMON_ARCH='-march=i486 -mtune=bonnell '
 
 RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=i486 -Clink-args=-latomic '
