23c939cabd016597aa639f0cd602d6d3f623603e
Mingcong Bai <jeffbai@aosc.xyz>
2023-09-21 12:55:35 -0700

    arch/loongarch64: tweak default compiler flags
    - Change default target la464 => loongarch64, as la464 is a core, not a
      generic target.
    - -march=la464 is also known to generate LASX code, which is not compatible
      with some 2K-series chips (2K1000LA/2K2000).
    - Enable LSX extension by default, which is known to be available for all
      LoongArch-based chips.
    
    Ref: https://github.com/loongson/la-softdev-convention/blob/master/la-softdev-convention.adoc#73-vector-instruction-support
    Ref: https://github.com/loongson/la-softdev-convention/blob/master/la-softdev-convention.adoc#101-desktop-and-server-operating-system-build-requirements

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -2,7 +2,7 @@
 ##arch/loongarch64.sh: Build definitions for Loongson 3A/B 5000.
 ##@copyright GPL-2.0+
 
-CFLAGS_GCC_ARCH='-mabi=lp64d -march=la464 -mtune=la464'
+CFLAGS_GCC_ARCH='-mabi=lp64d -march=loongarch64 -mtune=la464 -mlsx'
 
 # Position-independent executable buildmode is not available on the LoongArch
 # architecture. Removing for the loongarch64 (loong64) target.
