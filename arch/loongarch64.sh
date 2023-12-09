3d64896a4c7841e369984c1b017f713a2efcee2b
liushuyu <liushuyu011@gmail.com>
2023-12-09 16:06:17 -0700

    arch/loongarch64.sh: add -flto-partition=none ...
    ... to workaround an issue with the linker when LTO is enabled

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -2,7 +2,7 @@
 ##arch/loongarch64.sh: Build definitions for Loongson 3A/B 5000.
 ##@copyright GPL-2.0+
 
-CFLAGS_GCC_ARCH='-mabi=lp64d -march=loongarch64 -mtune=la464 -mlsx'
+CFLAGS_GCC_ARCH='-mabi=lp64d -march=loongarch64 -mtune=la464 -mlsx -flto-partition=none'
 
 # Position-independent executable buildmode is not available on the LoongArch
 # architecture. Removing for the loongarch64 (loong64) target.
