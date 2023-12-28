fb4431dde57e15088a329ba3a0a8a9aef8cae728
Mingcong Bai <jeffbai@aosc.xyz>
2023-12-28 13:18:08 -0800

    arch/loongarch64.sh: disable vector support for now
    FIXME: +lsx feature is broken as of LLVM 17.0.6, awaiting full vector
    implementation.

--- arch/loongarch64.sh
+++ arch/loongarch64.sh
@@ -8,4 +8,6 @@ CFLAGS_GCC_ARCH='-mabi=lp64d -march=loongarch64 -mtune=la464 -mlsx'
 # architecture. Removing for the loongarch64 (loong64) target.
 GOFLAGS=${GOFLAGS/-buildmode=pie/}
 
-RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=generic-la64 -Ctarget-feature=+lsx,+d -Clink-arg=-mabi=lp64d'
+# FIXME: +lsx feature is broken as of LLVM 17.0.6, awaiting full vector
+# implementation.
+RUSTFLAGS_COMMON_ARCH='-Ctarget-cpu=generic-la64 -Ctarget-feature=-lsx,+d -Clink-arg=-mabi=lp64d'
